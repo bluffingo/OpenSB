e38bc6ccf1690f17f200f79da2ec74a62ac7e5e0
PF94 <45898787+chaziz94@users.noreply.github.com>
2021-04-25 17:17:02 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require('lib/common.php');
 // currently selects all uploaded videos, should turn it into all featured only
 $videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, v.views, v.author FROM videos v JOIN users u ON v.author = u.id ORDER BY v.id DESC");
 
-$twig = twigloader();
+$twig = twigloader(); //please rewrite the code to not use twig, it's clunky for me and bazingfag doesn't like it
 
 echo $twig->render('index.twig', [
 	'videos' => $videoData
