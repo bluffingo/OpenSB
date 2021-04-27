eb0157065acde6fb6271994f0d43459865e721a7
forkiesassds <icanttellyou@memeware.net>
2021-04-27 08:13:22 +0300

    make user page actually show data from db

--- user.php
+++ user.php
@@ -1,6 +1,12 @@
 <?php
 require($_SERVER['DOCUMENT_ROOT'] . '/lib/common.php');
 
+$username = (isset($_GET['name']) ? $_GET['name'] : null);
+
+$userData = fetch("SELECT * FROM users WHERE username = ?", [$username]);
+
 $twig = twigloader();
 
-echo $twig->render('user.twig');
+echo $twig->render('user.twig', [
+    'user' => $userData,
+]);
