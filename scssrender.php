a1e02f6a4575cfe2f77110e1efc464c30bf4ea29
forkiesassds <icanttellyou@memeware.net>
2021-05-06 17:35:21 +0300

    i hope this won't be all full of security errors...

--- scssrender.php
+++ scssrender.php
@@ -10,19 +10,19 @@ if(!isset($_GET['path'])) {
 $scss = new Compiler();
 $scss->setImportPaths($_SERVER['DOCUMENT_ROOT']);
 if (file_exists('cache/'.substr($_GET['path'], 0, strlen($_GET['path']) - 4).'css') AND $tplNoCache != true) {
-	$lines = file($_SERVER['DOCUMENT_ROOT'] . '/cache/'.substr($_GET['path'], 0, strlen($_GET['path']) - 4).'css');
+	$lines = file(realpath($_SERVER['DOCUMENT_ROOT'] . '/cache/'.substr($_GET['path'], 0, strlen($_GET['path']) - 4).'css'));
 	foreach ($lines as $line_num => $line) {
 		echo $line;
 	}
 } else {
 	$css = $scss->compile('@import "'.$_GET['path'].'"');
 	if($tplNoCache != true) {
-		$parts = explode('/', $_SERVER['DOCUMENT_ROOT'] . '/cache/'.substr($_GET['path'], 0, strlen($_GET['path']) - 4).'css');
+		$parts = explode('/', 'cache/'.substr($_GET['path'], 0, strlen($_GET['path']) - 4).'css');
 		array_pop($parts);
 		$dir = implode('/', $parts);
 		if(!is_dir($dir))
 			mkdir($dir);
-		$file = fopen($_SERVER['DOCUMENT_ROOT'] . '/cache/'.substr($_GET['path'], 0, strlen($_GET['path']) - 4).'css', 'w');
+		$file = fopen('cache/'.substr($_GET['path'], 0, strlen($_GET['path']) - 4).'css', 'w');
 		fwrite($file, $css);
 		fclose($file);
 	}
