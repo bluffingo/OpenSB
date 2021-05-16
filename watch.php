a93c950d9c71cff6bbb4502b2b38e1580cfcbf1c
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-05-16 17:18:18 +0200

    Increment video views.

--- watch.php
+++ watch.php
@@ -7,9 +7,12 @@ $videoData = fetch("SELECT $userfields v.* FROM videos v JOIN users u ON v.autho
 $commentData = query("SELECT $userfields c.id, c.comment, c.author, c.date, c.deleted FROM comments c JOIN users u ON c.author = u.id WHERE c.id = ? ORDER BY c.date DESC", [$id]);
 $relatedVideosData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, v.views, v.author FROM videos v JOIN users u ON v.author = u.id ORDER BY v.id DESC");
 
+query("UPDATE videos SET views = views + '1' WHERE video_id = ?", [$id]);
+$videoData['views']++;
+
 $twig = twigloader();
 echo $twig->render('watch.twig', [
     'video' => $videoData,
-    'related_videos' => $relatedVideosData, 
+    'related_videos' => $relatedVideosData,
 	'comments' => $commentData
 ]);
