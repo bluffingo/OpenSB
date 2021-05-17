f015b8291e00770b40dd31dadae6749020031d0c
veselcraft <veselcraft@icloud.com>
2021-05-17 18:53:34 +0300

    using getcwd() instead of ['DOCUMENT_ROOT'], updating .gitignore file to include admin tools like "adminer"

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require($_SERVER['DOCUMENT_ROOT'] . '/lib/common.php');
+require(getcwd() . '/lib/common.php');
 
 // currently selects all uploaded videos, should turn it into all featured only
 $videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, v.views, v.author FROM videos v JOIN users u ON v.author = u.id ORDER BY v.id DESC");
