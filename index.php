e7153693a098a03a34b898c6e0694e1dad14dd50
forkiesassds <icanttellyou@memeware.net>
2021-06-09 18:16:00 +0300

    implement user stats

--- index.php
+++ index.php
@@ -4,9 +4,18 @@ require('lib/common.php');
 // currently selects all uploaded videos, should turn it into all featured only
 $videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, v.views, v.author FROM videos v JOIN users u ON v.author = u.id ORDER BY v.id DESC");
 $featuredVideoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, v.views, v.author FROM videos v JOIN users u ON v.author = u.id WHERE flags = 1 ORDER BY v.id DESC"); //i have no clue how should flags even work.
+if ($loggedIn) {
+	$totalViews = result("SELECT SUM(views) FROM videos WHERE author = ?", [$currentUser['id']]);
+	$creationDate = result("SELECT joined FROM users WHERE id = ?", [$currentUser['id']]);
+} else {
+	$totalViews = 0;
+	$creationDate = 0;
+}
 $twig = twigloader();
 
 echo $twig->render('index.twig', [
 	'videos' => $videoData,
-	'featuredVideos' => $featuredVideoData
+	'featuredVideos' => $featuredVideoData,
+	'totalViews' => $totalViews,
+	'creationDate' => $creationDate
 ]);
