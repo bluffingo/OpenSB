2efadd199bd7fa883ed6e702ee9b526a5c1511da
forkiesassds <icanttellyou@memeware.net>
2021-06-17 14:56:48 +0300

    space out strings from each file
    might have to reimplement sorting, but sorting it kills the spacing.

--- tools/translation-generator.py
+++ tools/translation-generator.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import re
-from natsort import natsorted
+import platform
 
 exts = [".twig", ".php"]
 paths = [p for p in Path("./").rglob('*') if p.suffix in exts]
@@ -9,24 +9,33 @@ strings = []
 
 for path in paths:
 	# Ignore composer dependency files
-	if "vendor/" in str(path): continue
+	if platform.system() == "Windows":
+		if "vendor\\" in str(path): continue
+		if "templates\\cache\\" in str(path): continue
+	else:
+		if "vendor/" in str(path): continue
+		if "templates/cache/" in str(path): continue
 
-	with open(path, "r") as f:
+	with open(path, "r", encoding='Latin-1') as f:
 		file_matches = re.findall('__\("([^"]+)"', f.read())
-
-		for match in file_matches:
+		if len(file_matches):
+			if len(strings):
+				if strings[-1] != "\n":
+					strings.append("\n")
+        
+		for match in file_matches:	
 			# prevent duplicate strings (i.e. 6 "views")
 			if match in strings:
 				continue
 			strings.append(match)
 
-# Give the strings a reproducible order
-strings = natsorted(strings, reverse=False)
-
 with open("lib/lang/template_new.json", "w") as f:
 	f.write("{\n")
 	i = 0
 	for string in strings:
+		if string == "\n":
+			f.write("\n")
+			continue
 		i += 1
 		f.write('\t"%s": ""' % string)
 		if not len(strings) == i:
