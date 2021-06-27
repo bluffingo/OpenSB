4e1958503a689636274787c473d411e7b3ba3310
forkiesassds <icanttellyou@memeware.net>
2021-06-27 14:20:21 +0300

    add search

---
+++ search.php
@@ -0,0 +1,14 @@
+<?php
+require('lib/common.php');
+
+$query = isset($_GET['query']) ? $_GET['query'] : null;
+
+// currently selects all uploaded videos
+$videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, v.views, v.author, v.tags FROM videos v JOIN users u ON v.author = u.id WHERE v.tags LIKE CONCAT('%', ?, '%') OR v.title LIKE CONCAT('%', ?, '%') OR v.description LIKE CONCAT('%', ?, '%') ORDER BY v.id DESC", [$query, $query, $query]);
+
+$twig = twigloader();
+
+echo $twig->render('search.twig', [
+	'videos' => $videoData,
+	'query' => $query
+]);
