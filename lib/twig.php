3cd88a1fc31c3fe9d65720d76a4ffee956c7c179
forkiesassds <icanttellyou@memeware.net>
2021-06-29 21:56:45 +0300

    show video tags

--- lib/twig.php
+++ lib/twig.php
@@ -20,6 +20,7 @@ class SBExtension extends \Twig\Extension\AbstractExtension {
 	public function getFilters() {
 		return [
 			new \Twig\TwigFilter('relative_time', 'relativeTime'),
+			new \Twig\TwigFilter('json_decode', 'jsonDecode'),
 		];
 	}
 }
