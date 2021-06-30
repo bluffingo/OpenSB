e2ed896ba5e5329a86a2d8ecf7c54a20f7d6317d
forkiesassds <icanttellyou@memeware.net>
2021-06-30 20:05:09 +0300

    implement basic watch_ajax support

---
+++ watch_ajax.php
@@ -0,0 +1,5 @@
+<?php 
+header('Content-type: application/xml'); 
+?>
+<?xml version="1.0" encoding="UTF-8"?>
+<root><html_content><![CDATA[<table class="showingTable">watch_ajax.php test</table>]]></html_content><return_code><![CDATA[0]]></return_code></root>
