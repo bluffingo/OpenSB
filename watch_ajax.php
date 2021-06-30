122aec2af1f94dc18fc9b1e46f0e361f5d0dce84
Chaziz <45898787+flickPF94@users.noreply.github.com>
2021-06-30 14:34:49 -0400

    fix ajax breaking on my side, and remove "hours" from runtime

--- watch_ajax.php
+++ watch_ajax.php
@@ -1,5 +1,5 @@
 <?php 
 header('Content-type: application/xml'); 
+echo '<?xml version="1.0" encoding="UTF-8"?>'; //fixes my php from dying. -gamerappa 6/30/2021
 ?>
-<?xml version="1.0" encoding="UTF-8"?>
-<root><html_content><![CDATA[<table class="showingTable">watch_ajax.php test</table>]]></html_content><return_code><![CDATA[0]]></return_code></root>
+<root><html_content><![CDATA[<table class="showingTable">2006L AJAX. Not currently finished.</table>]]></html_content><return_code><![CDATA[0]]></return_code></root>
