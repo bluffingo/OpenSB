8aa365c883c17d191d57171074c1b05fa5309371
forkiesassds <icanttellyou@memeware.net>
2021-07-06 23:52:28 +0300

    fix pagination

--- browse.php
+++ browse.php
@@ -1,11 +1,9 @@
 <?php
 require('lib/common.php');
-
-$min = ((isset($_GET['page']) ? $_GET['page'] : 1) - 1) * 20;
-$max = $min + 20;
+$offset = ((isset($_GET['page']) ? $_GET['page'] : 1) - 1) * 20;
 
 // currently selects all uploaded videos
-$videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, v.views, v.author FROM videos v JOIN users u ON v.author = u.id WHERE NOT v.flags = 0010 AND NOT v.flags = 0020 ORDER BY v.id DESC LIMIT ?, ?", [$min, $max]);
+$videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, v.views, v.author FROM videos v JOIN users u ON v.author = u.id WHERE NOT v.flags = 0010 AND NOT v.flags = 0020 ORDER BY v.id DESC LIMIT 20 OFFSET ?", [$offset]);
 
 $pageCount = ceil(fetch("SELECT COUNT(*) FROM videos")['COUNT(*)'] / 20);
 $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'].'/browse.php?page=';
