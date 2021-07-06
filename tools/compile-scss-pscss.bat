6dc5d2fb8369ba9278e7d26bfc1b9a212f997319
PF94 <45898787+chaziz94@users.noreply.github.com>
2021-07-06 13:08:07 -0400

    Update compile-scss-pscss.bat

--- tools/compile-scss-pscss.bat
+++ tools/compile-scss-pscss.bat
@@ -4,7 +4,8 @@ set pscss_path="vendor/scssphp/scssphp/bin/pscss"
 
 :: "Compressed" style gives lowest filesize
 :: Load path is assuming you're running this script from the root of the sB site directory
-set common_arguments="--style=compressed --load-path=./"
+set common_arguments="--style=compressed"
 
 php %pscss_path% %common_arguments% bootstrap/bs.scss > assets/bs.css
 php %pscss_path% %common_arguments% bootstrap/bs-dark.scss > assets/bs-dark.css
+php %pscss_path% %common_arguments% bootstrap/bs-classicish.scss > assets/bs-classicish.css
