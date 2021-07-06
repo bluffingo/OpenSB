e56765ade50619140720469646fa67803e15d321
forkiesassds <icanttellyou@memeware.net>
2021-07-07 00:39:58 +0300

    add dartsass compile scripts.

---
+++ tools/compile-scss-dartsass.bat
@@ -0,0 +1,8 @@
+@echo off
+
+:: "Compressed" style gives lowest filesize
+:: Load path is assuming you're running this script from the root of the sB site directory
+set common_arguments="--style=compressed"
+
+sass %common_arguments% bootstrap/bs.scss assets/bs.css
+sass %common_arguments% bootstrap/bs-dark.scss assets/bs-dark.css
---
+++ tools/compile-scss-dartsass.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# "Compressed" style gives lowest filesize
+# Load path is assuming you're running this script from the root of the sB site directory
+common_arguments="--style compressed --load-path ./"
+
+sass ${common_arguments} bootstrap/bs.scss assets/bs.css
+sass ${common_arguments} bootstrap/bs-dark.scss assets/bs-dark.css
