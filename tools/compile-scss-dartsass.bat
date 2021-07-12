2027fa44f35230fd532867a02e71e9a81773d5e3
forkiesassds <icanttellyou@memeware.net>
2021-07-12 23:39:59 +0300

    implement hack to fix hover fade effect

--- tools/compile-scss-dartsass.bat
+++ tools/compile-scss-dartsass.bat
@@ -1,8 +1,7 @@
-@echo off
+REM @echo off
 
 :: "Compressed" style gives lowest filesize
 :: Load path is assuming you're running this script from the root of the sB site directory
 set common_arguments="--style=compressed"
 
-sass %common_arguments% bootstrap/bs.scss assets/bs.css
-sass %common_arguments% bootstrap/bs-dark.scss assets/bs-dark.css
+sass %common_arguments% bootstrap/bs.scss:assets/bs.css bootstrap/bs-dark.scss:assets/bs-dark.css
