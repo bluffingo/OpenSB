c366b8a5e96b86dacc30958de12fb8c705b4ea3a
forkiesassds <icanttellyou@memeware.net>
2021-07-17 23:58:06 +0300

    fix batch version of sbnext compiler

--- tools/compile-scss-sbnext-dartsass.bat
+++ tools/compile-scss-sbnext-dartsass.bat
@@ -2,6 +2,6 @@
 
 :: "Compressed" style gives lowest filesize
 :: Load path is assuming you're running this script from the root of the sB site directory
-set common_arguments="--style=compressed --watch"
+set common_arguments="--style=compressed"
 
-sass %common_arguments% assets/main.scss:assets/main.css
+sass %common_arguments% --watch assets/main.scss:assets/main.css
