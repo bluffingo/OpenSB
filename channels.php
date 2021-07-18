5ab6a88ee99e42f95220829faad12801c5961aec
forkiesassds <icanttellyou@memeware.net>
2021-07-18 23:16:14 +0300

    limit and offset in channels page

--- channels.php
+++ channels.php
@@ -1,11 +1,10 @@
 <?php
 require('lib/common.php');
 
-$min = ((isset($_GET['page']) ? $_GET['page'] : 1) - 1) * 20;
-$max = $min + 20;
+$offset = ((isset($_GET['page']) ? $_GET['page'] : 1) - 1) * 20;
 
 // currently selects all registered users (channels)
-$userData = query("SELECT username, lastview FROM users ORDER BY lastview DESC");
+$userData = query("SELECT username, lastview FROM users ORDER BY lastview DESC LIMIT 20 OFFSET ?", [$offset]);
 
 $pageCount = ceil(fetch("SELECT COUNT(*) FROM users")['COUNT(*)'] / 20);
 $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'].'/channels.php?page=';
