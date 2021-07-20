cc6dae7f39a6190306755557ac90833bce7d843e
Chaziz <chazpelo2@gmail.com>
2021-07-20 14:30:56 -0400

    partnership form + user api

---
+++ api/v1/user.php
@@ -0,0 +1,29 @@
+<?php
+chdir('../../');
+$rawOutputRequired = true;
+require('lib/common.php');
+
+header('Content-Type: application/json');
+
+$username = (isset($_GET['name']) ? $_GET['name'] : null);
+
+$userData = fetch("SELECT * FROM users WHERE username = ?", [$username]);
+
+if (!$userData) {
+	$apiOutput = [ 'error' => "No user specified or invalid video ID", 'code' => "52e44102" ];
+
+	echo json_encode($apiOutput);
+	die();
+}
+
+// TODO: comments? likes?
+$apiOutput = [
+	'id'	=> $userData['id'],
+	'username'	=> $userData['username'],
+	'joinDate' => $userData['joined'],
+	'lastConnection' => $userData['lastview'],
+	'profileColor' => $userData['color'],
+	'description' => $userData['description'],
+];
+
+echo json_encode($apiOutput);
