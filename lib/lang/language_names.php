6433f056257afb744638563dc4e5a12470577942
forkiesassds <icanttellyou@memeware.net>
2021-07-21 03:05:22 +0300

    don't show pseudo locale when debug mode is not enabled

--- lib/lang/language_names.php
+++ lib/lang/language_names.php
@@ -1,10 +1,12 @@
 <?php
-
+require('conf/config.php');
 // Each corresponding language should have its local language name.
 $languages = [
 	'en_US' => "English",
 	'fr_CA' => "Français (Canada)",
 	'ru_RU' => "Русский",
 	'sv_SE' => "Svenska",
-	'qps-plocm' => "Pseudolocalization [DEBUG ONLY]",
 ];
+if ($isDebug) {
+	$languages['qps_plocm'] = "Pseudolocalization [DEBUG ONLY]";
+}
