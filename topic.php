4439ce4d3afc98658771e42d36f2ec2fea0643c0
Gamerappa <chazpelo2@gmail.com>
2021-07-30 16:56:13 -0400

    [Forums] No one cares about forums...

--- topic.php
+++ topic.php
@@ -1,12 +1,15 @@
 <?php
 require('lib/common.php');
 
+//no one is interested so don't expect this to be done until about beta 2
+//-gr 7/30/2021
+
 $postdatatest = [
 	'id' => "1",
 	'category' => "General",
 	'title' => "Example Thread",
 	'description' => "Holy shit are people uninterested.",
-	'author' => "Katt Wallace",
+	'author' => 1,
 	'views' => 342213,
 	'date' => "15 minutes ago"
 ];
