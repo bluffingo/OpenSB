1b2d8f5640b4216442022361a9a2396f849683ae
forkiesassds <icanttellyou@memeware.net>
2021-08-01 00:17:53 +0300

    fix sql query and remove useless echo

--- topic.php
+++ topic.php
@@ -11,7 +11,7 @@ $id = (isset($_GET['thread']) ? $_GET['thread'] : null);
 // doesn't fuckin' work, is the only solution just about using some shitty retarded forum hosting service from usa?
 // fuck off. not dealing with slow-ass outdated software from fucking 2006.
 // no icty, this wasnt based on fucking finobe nor blockland. POKTUBE HAD FORUMS. SBNEXT IS NOT AN EXCUSE.
-$postData = query("SELECT * FROM posts WHERE thread_id = ?", [$id]);
+$postData = query("SELECT * FROM posts WHERE id = ?", [$id]);
 
 //no one is interested so don't expect this to be done until about beta 2
 //-gr 7/30/2021
@@ -20,5 +20,3 @@ $twig = twigloader();
 echo $twig->render('topic.twig', [
 	'post_data' => $postData
 ]);
-
-echo $id;
