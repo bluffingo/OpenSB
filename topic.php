d6dd7a58d9f632bfca7afbf00551dc804f8eebbe
Gamerappa <chazpelo2@gmail.com>
2021-07-31 16:15:38 -0400

    Fuck this shit.

--- topic.php
+++ topic.php
@@ -8,22 +8,17 @@ if (!$isDebug) {
 
 $id = (isset($_GET['thread']) ? $_GET['thread'] : null);
 
-$postData = fetch("SELECT $userfields v.* FROM posts v JOIN users u ON v.author = u.id WHERE v.id = ?", [$id]);
+// doesn't fuckin' work, is the only solution just about using some shitty retarded forum hosting service from usa?
+// fuck off. not dealing with slow-ass outdated software from fucking 2006.
+// no icty, this wasnt based on fucking finobe nor blockland. POKTUBE HAD FORUMS. SBNEXT IS NOT AN EXCUSE.
+$postData = query("SELECT * FROM posts WHERE thread_id = ?", [$id]);
 
 //no one is interested so don't expect this to be done until about beta 2
 //-gr 7/30/2021
-
-$postdatatest = [
-	'id' => "1",
-	'category' => "General",
-	'title' => "Example Thread",
-	'description' => "Holy shit are people uninterested.",
-	'author' => 1,
-	'views' => 342213,
-	'date' => "15 minutes ago"
-];
 
 $twig = twigloader();
 echo $twig->render('topic.twig', [
 	'post_data' => $postData
 ]);
+
+echo $id;
