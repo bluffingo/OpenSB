832a92f96af67a943748297dbc77d8b64596b0fb
forkiesassds <icanttellyou@memeware.net>
2021-08-02 16:56:29 +0300

    STOP FORGETTING THE OTHER COMPILERS

--- tools/compile-scss-sassc.sh
+++ tools/compile-scss-sassc.sh
@@ -8,3 +8,4 @@ sassc ${common_arguments} bootstrap/bs.scss assets/bs.css
 sassc ${common_arguments} bootstrap/bs-dark.scss assets/bs-dark.css
 sassc ${common_arguments} bootstrap/bs-finalium.scss assets/bs-finalium.css
 sassc ${common_arguments} bootstrap/bs-finalium-dark.scss assets/bs-finalium-dark.css
+sassc ${common_arguments} bootstrap/bs-vanilla.scss assets/bs-vanilla.css
