04f6168bf5d39db97b11cb17e23091f3a1a02e6e
Gamerappa <chazpelo2@gmail.com>
2021-08-26 18:24:47 -0400

    fix search bar coloring issue whatever
    closes #65

--- tools/compile-scss-pscss.sh
+++ tools/compile-scss-pscss.sh
@@ -6,13 +6,13 @@ pscss_path="vendor/scssphp/scssphp/bin/pscss"
 # Load path is assuming you're running this script from the root of the sB site directory
 common_arguments="--style=compressed"
 
-echo "Compiling Classicish Light"
-php ${pscss_path} ${common_arguments} bootstrap/bs.scss > assets/bs.css
-echo "Compiling Classicish Dark"
-php ${pscss_path} ${common_arguments} bootstrap/bs-dark.scss > assets/bs-dark.css
 echo "Compiling Finalium Light"
 php ${pscss_path} ${common_arguments} bootstrap/bs-finalium.scss > assets/bs-finalium.css
 echo "Compiling Finalium Dark"
 php ${pscss_path} ${common_arguments} bootstrap/bs-finalium-dark.scss > assets/bs-finalium-dark.css
+echo "Compiling Classicish Light"
+php ${pscss_path} ${common_arguments} bootstrap/bs.scss > assets/bs.css
+echo "Compiling Classicish Dark"
+php ${pscss_path} ${common_arguments} bootstrap/bs-dark.scss > assets/bs-dark.css
 echo "Compiling Customized Vanilla"
 php ${pscss_path} ${common_arguments} bootstrap/bs-vanilla.scss > assets/bs-vanilla.css
