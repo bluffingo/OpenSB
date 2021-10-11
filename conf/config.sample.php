ae9b3b174da009a417562f91289b20f1131432db
Gamerappa <45898787+chaziz94@users.noreply.github.com>
2021-10-11 13:56:19 -0400

    change template to sbnext for config template
    this is due to major upcoming breaking change

--- conf/config.sample.php
+++ conf/config.sample.php
@@ -25,7 +25,7 @@ $testNewLayout = false; // DEV ONLY, deprecated
 $darkModeDefault = true;
 
 // TEMPLATE OPTIONS (redone 9/17/2021 by gr):
-$useTemplate = "default" // check the templates folder for available options
+$useTemplate = "prototype" // check the templates folder for available options
 $oldTemplateSwitching = false // old alpha 3.5 template switching
 
 // hCaptcha secret and sitekey,
