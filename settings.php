5ba494fec2d27102064328909df7920bd5b6bb6d
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-10-13 17:34:47 +0200

    Rework settings backend for new frontend, add some more squareBracket-specific settings.

--- settings.php
+++ settings.php
@@ -2,80 +2,79 @@
 require('lib/common.php');
 use Intervention\Image\ImageManager;
 
-$manager = new ImageManager();
-
 if (!$log) redirect('login.php');
 
-if (isset($_POST['updatesettings'])) {
+$error = '';
+
+if (isset($_POST['magic'])) {
 	$displayName	= isset($_POST['displayName']) ? $_POST['displayName'] : null;
-	$description	= isset($_POST['description']) ? $_POST['description'] : null;
 	$color			= isset($_POST['color']) ? $_POST['color'] : '#523bb8'; // setting color to "null" would fuck up the scss compiler(?) -gr 7/26/2021
+	$description	= isset($_POST['description']) ? $_POST['description'] : null;
+	$signature		= isset($_POST['signature']) ? $_POST['signature'] : null;
+
+	$darkmode		= isset($_POST['darkmode']) ? $_POST['darkmode'] : false;
 	$language		= isset($_POST['language']) ? $_POST['language'] : 'en-US';
-	
-	$resetToken		= isset($_POST['reset_token']) ? $_POST['reset_token'] : null;
-	
+
 	$currentPass    = (isset($_POST['current_pass']) ? $_POST['current_pass'] : null);
 	$pass           = (isset($_POST['pass']) ? $_POST['pass'] : null);
 	$pass2          = (isset($_POST['pass2']) ? $_POST['pass2'] : null);
-	
-	$error = "";
-	if (!$currentPass) $error .= __("do not reset"); // Placeholder strings? -gr 7/26/2021
-	if (!$pass) $error .= __("do not reset");
-	if (!$pass2) $error .= __("do not reset");
-	if ($pass != $pass2) $error .= __("Passwords aren't identical.");
-	
-	$logindata = fetch("SELECT password FROM users WHERE id = ?", [$userdata['id']]);
-	if ($logindata && password_verify($currentPass, $logindata['password'])) {
-		if ($error == '') {
-			query("UPDATE users SET password = ?, token = ? WHERE id = ?", 
-				[password_hash($pass, PASSWORD_DEFAULT), bin2hex(random_bytes(32)), $userdata['id']]);
 
-			redirect('login.php?new_pass');
+	if ($currentPass && $pass && $pass2) {
+		if (password_verify($currentPass, $userdata['password'])) {
+			if ($pass == $pass2) {
+				query("UPDATE users SET password = ?, token = ? WHERE id = ?",
+					[password_hash($pass, PASSWORD_DEFAULT), bin2hex(random_bytes(32)), $userdata['id']]);
+
+				redirect('login.php?new_pass');
+			} else {
+				$error .= __(" The new passwords aren't identical.");
+			}
+		} else {
+			$error .= __("Your current password is incorrect.");
 		}
 	}
-	
-	if($resetToken) {
-		query("UPDATE users SET token = ? WHERE id = ?", [bin2hex(random_bytes(32)), $userdata['id']]);
-		redirect('login.php?new_token');
-	}
-	
-	$name       = $_FILES['profilePicture']['name'];
-	$temp_name  = $_FILES['profilePicture']['tmp_name'];
-	$ext  = pathinfo( $_FILES['profilePicture']['name'], PATHINFO_EXTENSION );
-	$target_file = 'assets/profpic/' . $userdata['name'] . '.png';
-	if (move_uploaded_file($temp_name, $target_file)){
-		// Back in PokTube there was a debate over if we should make profiles pictures use 1:1.
-		// The result was to not resize strech profile pictures. That was back when PokTube
-		// wasn't going to be a modern site (other than the Semantic UI bullshit). however
-		// squareBracket is a modern site so any non-1:1 profile pictures should be strected 
-		// to 1:1. there are some non-1:1 profile pictures being used on squarebracket but they 
-		// came from PokTube user data getting migrated (included profile pictures). Just like 
-		// how Twitter still keeps GIF profile pictures for those who haven't changed their profile
-		// picture to a new static one. We should keep PokTube-migrated non-1:1 profile pictures.
-		//                                                                 -Gamerappa, 7/26/2021
-		$img = $manager->make($target_file);
-		$img->resize(640, 640);
-		$img->save($target_file, 0, 'png');
-	}
-	$backname       = $_FILES['profileBackground']['name'];
-	$backtemp_name  = $_FILES['profileBackground']['tmp_name'];
-	$backext  = pathinfo( $_FILES['profileBackground']['name'], PATHINFO_EXTENSION );
-	$backtarget_file = 'assets/backgrounds/' . $userdata['name'] . '.png';
-	if (move_uploaded_file($backtemp_name, $backtarget_file)) {
-		$img = $manager->make($backtarget_file);
-		$img->save($backtarget_file, 0, 'png');
-	}
-	query("UPDATE users SET title = ?, description = ?, customcolor = ?, language = ? WHERE id = ?",
-		[$displayName, $description, $color, $language, $userdata['id']]);
+	if ($error) $error = "The following errors occured while changing your password: ".$error;
 
-	redirect(sprintf("user.php?name=%s&edited", $userdata['name']));
+	//  Code related to profile picture uploading
+	if (isset($_FILES['profilePicture'])) {
+		$manager = new ImageManager();
+		$name       = $_FILES['profilePicture']['name'];
+		$temp_name  = $_FILES['profilePicture']['tmp_name'];
+		$ext  = pathinfo( $_FILES['profilePicture']['name'], PATHINFO_EXTENSION );
+		$target_file = 'assets/profpic/' . $userdata['name'] . '.png';
+		if (move_uploaded_file($temp_name, $target_file)){
+			// Back in PokTube there was a debate over if we should make profiles pictures use 1:1.
+			// The result was to not resize strech profile pictures. That was back when PokTube
+			// wasn't going to be a modern site (other than the Semantic UI bullshit). however
+			// squareBracket is a modern site so any non-1:1 profile pictures should be strected
+			// to 1:1. there are some non-1:1 profile pictures being used on squarebracket but they
+			// came from PokTube user data getting migrated (included profile pictures). Just like
+			// how Twitter still keeps GIF profile pictures for those who haven't changed their profile
+			// picture to a new static one. We should keep PokTube-migrated non-1:1 profile pictures.
+			//                                                                 -Gamerappa, 7/26/2021
+			$img = $manager->make($target_file);
+			$img->resize(640, 640);
+			$img->save($target_file, 0, 'png');
+		}
+		$backname       = $_FILES['profileBackground']['name'];
+		$backtemp_name  = $_FILES['profileBackground']['tmp_name'];
+		$backext  = pathinfo( $_FILES['profileBackground']['name'], PATHINFO_EXTENSION );
+		$backtarget_file = 'assets/backgrounds/' . $userdata['name'] . '.png';
+		if (move_uploaded_file($backtemp_name, $backtarget_file)) {
+			$img = $manager->make($backtarget_file);
+			$img->save($backtarget_file, 0, 'png');
+		}
+	}
+
+	query("UPDATE users SET title = ?, description = ?, customcolor = ?, language = ?, signature = ?, darkmode = ? WHERE id = ?",
+		[$displayName, $description, $color, $language, $signature, $darkmode, $userdata['id']]);
+
+	if (!$error) {
+		redirect(sprintf("user.php?name=%s&edited", $userdata['name']));
+	}
 }
 
 $twig = twigloader();
-if ($testNewLayout) {
-	// COMPLETE (yes) revamp of the whole settings page, could make changing settings unusable so 
-	// DEV ONLY -gr 7/26/2021 (holy shit the gamerappa name is 5 years old now)
-	echo $twig->render('settings_redesign.twig'); 
-} else {
-	echo $twig->render('settings.twig');
-}
+echo $twig->render('settings.twig', [
+	'error' => isset($error) ? $error : null
+]);
