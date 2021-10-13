a74a221a76041ec8975a2ed48d43fcdf8eeee81f
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-10-13 18:47:32 +0200

    Fix sample config.

--- conf/config.sample.php
+++ conf/config.sample.php
@@ -19,16 +19,14 @@ $domain = 'http://squarebracket.local';
 $isDebug = false; // DEV ONLY
 $isMaintenance = false;
 
-$testNewLayout = false; // DEV ONLY, deprecated
-
 // Should dark mode be default? (FORUM ONLY)
 $darkModeDefault = true;
 
 $lpp = 20; //Fuck.
 
 // TEMPLATE OPTIONS (redone 9/17/2021 by gr):
-$useTemplate = "prototype" // check the templates folder for available options
-$oldTemplateSwitching = false // old alpha 3.5 template switching
+$useTemplate = "prototype"; // check the templates folder for available options
+$oldTemplateSwitching = false; // old alpha 3.5 template switching
 
 // hCaptcha secret and sitekey,
 // if these are empty, hCaptcha is disabled.
