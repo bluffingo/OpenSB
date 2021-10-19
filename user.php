d143513bfa8bde4f0630a12133fe441ffcb38935
Gamerappa <chazpelo2@gmail.com>
2021-10-19 14:54:30 -0400

    header on profiles + early not-so-proper category list

--- user.php
+++ user.php
@@ -1,5 +1,6 @@
 <?php
 require('lib/common.php');
+use ScssPhp\ScssPhp\Compiler;
 
 if (isset($_GET['id'])) {
 	$userpagedata = fetch("SELECT * FROM users WHERE id = ?", [$_GET['id']]);
@@ -68,6 +69,36 @@ if (isset($userdata['id']) && $userdata['id'] == $userpagedata['id'] && !$forceu
 	//clearMentions('user', $userpagedata['id']);
 }
 
+$subCount = fetch("SELECT COUNT(user) FROM subscriptions WHERE user = ?", [$userpagedata['id']])['COUNT(user)'];
+
+$scss = new Compiler();
+$scss->setImportPaths($_SERVER['DOCUMENT_ROOT']);
+$css = $scss->compile(
+	'$color: '.$userpagedata['customcolor'].';
+	@mixin gradient-y-three-colors($start-color: $blue, $mid-color: $purple, $color-stop: 50%, $end-color: $red) {
+		background-image: linear-gradient($start-color, $mid-color $color-stop, $end-color);
+	}
+	@mixin text-contrast($n) {
+		$color-brightness: round((red($n) * 299) + (green($n) * 587) + (blue($n) * 114) / 1000);
+		$light-color: round((red(#ffffff) * 299) + (green(#ffffff) * 587) + (blue(#ffffff) * 114) / 1000);
+
+		@if abs($color-brightness) < ($light-color/2){
+			color: white;
+		}
+
+		@else {
+			color: black;
+		}
+	}
+	.bg-custom-profile {
+		@include gradient-y-three-colors(darken($color, 0%), darken($color, 7%), 50%, darken($color, 15%));
+		@include text-contrast($color);
+	}
+	.bg-primary {
+		@include gradient-y-three-colors(lighten($color, 8%), $color, 60%, darken($color, 4%));
+	}'
+);
+
 $twig = twigloader();
 echo $twig->render('user.twig', [
 	'id' => $userpagedata['id'],
@@ -80,5 +111,6 @@ echo $twig->render('user.twig', [
 	'notifs' => (isset($notifications) ? $notifications : []),
 	'markread' => (isset($_GET['markread']) ? true : false),
 	'edited' => (isset($_GET['edited']) ? true : false),
-	'comments' => (isset($comments) ? $comments : null)
+	'comments' => (isset($comments) ? $comments : null),
+	'subCount' => $subCount
 ]);
