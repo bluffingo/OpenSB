dcd7b20c07bae5412d14033116ebe9a051382f11
Gamerappa <chazpelo2@gmail.com>
2021-10-27 21:32:47 -0400

    yeah, ajax's broken, what do.
    blame userlink. fix this.

--- watch_ajax.php
+++ watch_ajax.php
@@ -1,10 +1,10 @@
-<?php
-header('Content-type: application/json'); 
+<?php 
 $rawOutputRequired = true;
+$videoID = $_GET['video_id'];
 foreach($_GET as $arg => $val) 
 {
 	switch($arg) {
-		case "action_load_system_feed":
+		case "action_get_related_videos_component":
 			$type = 0;
 			break;
 		case "action_get_related_playlist_component":
@@ -18,7 +18,7 @@ foreach($_GET as $arg => $val)
 require('lib/common.php');
 switch($type) {
 	case 0:
-		$videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, v.views, v.author, v.videolength FROM videos v JOIN users u ON v.author = u.id ORDER BY RAND() LIMIT 6");
+		$videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, v.views, v.videolength, v.tags, v.category_id, v.author FROM videos v JOIN users u ON v.author = u.id ORDER BY v.id DESC LIMIT 10");
 		break;
 	case 1: 
 		$videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, v.views, v.author, v.videolength FROM videos v JOIN users u ON v.author = u.id WHERE NOT v.video_id = ? AND NOT v.flags = 0010 AND NOT v.flags = 0020 ORDER BY RAND() LIMIT 0", [$videoID]);
@@ -30,16 +30,10 @@ switch($type) {
 		$videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, v.views, v.author, v.videolength FROM videos v JOIN users u ON v.author = u.id WHERE NOT v.video_id = ? AND NOT v.flags = 0010 AND NOT v.flags = 0020 ORDER BY RAND() LIMIT 6", [$videoID]);
 		break;
 }
+header('Content-type: application/xml'); 
 $twig = twigloader();
-?>
-{'paging': null, 'feed_html': `
-<div class=\'feed-container\' data-filter-type=\'\' data-view-type=\'\'>
-<div class=\'feed-page\'>
-<?php
-echo $twig->render('components/feed_list.twig', [
+echo $twig->render('components/ajaxsidebar.twig', [
 	'videos' => $videoData,
+	'type' => $type
 ]);
-?>
-</div>
-</div>
-`}
+
