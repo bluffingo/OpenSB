024d4dbb2f4bc1fabbdeef1aba7b96ae4d71461d
Gamerappa <chazpelo2@gmail.com>
2021-10-27 20:56:21 -0400

    guide ajax prototype

---
+++ guide_ajax.php
@@ -0,0 +1,8 @@
+<?php
+header('Content-Type: application/json');
+?>
+{'paging': null, 'feed_html': `
+<?php
+echo "Fuck, does the 2006L ajax code work on current sb codebase? check later -Gamerappa, October 27th 2021";
+?>
+`}
