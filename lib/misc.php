f73a11a2065892e67a1e9bd1ecbd805338e03027
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-11-06 20:11:19 +0100

    Get rid of noise when executing processingworker from the commandline.

--- lib/misc.php
+++ lib/misc.php
@@ -1,6 +1,24 @@
 <?php
 
 /**
+ * Returns true if it is executed from the command-line. (For command-line tools)
+ */
+function isCli() {
+	return php_sapi_name() == "cli";
+}
+
+// Makes incomplete unready features not available on production (aka squarebracket.veselcraft.ru)
+function notReady() {
+	http_response_code(403);
+	die(__("This feature is not ready for production."));
+}
+
+function accessDenied() {
+	http_response_code(403);
+	die(__("Access Denied"));
+}
+
+/**
  * Get hash of latest git commit
  *
  * @param bool $trim Trim the hash to the first 7 characters
