b4cbaa272ba5c7e94059bd7380f5ea851fcce217
Gamerappa <chazpelo2@gmail.com>
2021-11-08 12:51:43 -0500

    fix fucked css

--- tools/compile-scss-pscss.sh
+++ tools/compile-scss-pscss.sh
@@ -6,7 +6,9 @@ pscss_path="vendor/scssphp/scssphp/bin/pscss"
 # Load path is assuming you're running this script from the root of the sB site directory
 common_arguments="--style=compressed"
 
-echo "Compiling Forum Styles"
+echo "Compiling sbNext Styles"
 php ${pscss_path} ${common_arguments} assets/scss/style.scss > assets/css/style.css
-echo "Compiling Forum Dark Styles"
+echo "Compiling sbNext Dark Styles"
 php ${pscss_path} ${common_arguments} assets/scss/darkmode.scss > assets/css/darkmode.css
+echo "Compiling Embed Player Styles"
+php ${pscss_path} ${common_arguments} assets/scss/embed.scss > assets/css/embed.css
