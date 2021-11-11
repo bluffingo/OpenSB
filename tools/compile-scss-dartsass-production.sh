e04b6ec25fd2ac8cdea658eaefc20c1154061902
Gamerappa <chazpelo2@gmail.com>
2021-11-11 15:11:42 -0500

    Update compile-scss-dartsass-production.sh

--- tools/compile-scss-dartsass-production.sh
+++ tools/compile-scss-dartsass-production.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-common_arguments="--style extended --no-source-map --load-path ./"
+common_arguments="--style expanded --no-source-map --load-path ./"
 
 sass ${common_arguments} assets/scss/:assets/css/
