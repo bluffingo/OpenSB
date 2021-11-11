bbf6234221dad731327d72123f9872075bba47e4
Gamerappa <chazpelo2@gmail.com>
2021-11-11 14:54:57 -0500

    ??????????

---
+++ tools/compile-scss-dartsass-production.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# "Compressed" style gives lowest filesize
+# Load path is assuming you're running this script from the root of the sB site directory
+common_arguments="--style compressed --no-source-map --load-path ./"
+
+sass ${common_arguments} assets/scss/:assets/css/
--- tools/compile-scss-sassc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# "Compressed" style gives lowest filesize
-# Load path is assuming you're running this script from the root of the sB site directory
-common_arguments="--style compressed --load-path ./"
-
-sassc ${common_arguments} assets/scss/style.scss assets/css/style.css
-sassc ${common_arguments} assets/scss/darkmode.scss assets/css/darkmode.css
