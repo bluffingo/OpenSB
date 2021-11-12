ab18845fb8e375d1220eb9afdd0cf5970f94adcb
Gamerappa <chazpelo2@gmail.com>
2021-11-11 20:43:55 -0500

    Holy Shit

--- tools/compile-scss-dartsass-production.sh
+++ tools/compile-scss-dartsass-production.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-common_arguments="--style expanded --no-source-map --load-path ./"
+common_arguments="--style expanded --no-source-map --load-path ./  --watch"
 
 sass ${common_arguments} assets/scss/:assets/css/
