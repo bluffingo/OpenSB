f49bd9b8a9983afef9c6fcd883fff2475d151cff
Gamerappa <chazpelo2@gmail.com>
2021-11-26 22:27:30 -0500

    i have no idae

--- user.php
+++ user.php
@@ -2,6 +2,8 @@
 require('lib/common.php');
 use ScssPhp\ScssPhp\Compiler;
 
+$message = '';
+
 if (isset($_GET['id'])) {
 	$userpagedata = fetch("SELECT * FROM users WHERE id = ?", [$_GET['id']]);
 } else if (isset($_GET['name'])) {
@@ -9,6 +11,8 @@ if (isset($_GET['id'])) {
 }
 
 $customProfile = fetch("SELECT * FROM channel_settings WHERE user = ?", [$userpagedata['id']]);
+
+$page = isset($_GET['page']) ? $_GET['page'] : null;
 
 // var_dump($customProfile);
 
@@ -18,16 +22,37 @@ if ($customProfile == false) {
 	VALUES (?, '#ffffff', '#222222', '#ffffff', '#0033CC', '#ffffff', '#3399cc', '#ecf4fb', '#3399cc', '#ffffff')",[$userpagedata['id']]);
 }
 
+// using comment.php on 2008 would require clunky javascript
+if ($frontend = "2008") {
+	if ($page == "post_comment") {
+	$twig = twigloader();
+		echo $twig->render('writeProfileComment.twig', [
+			'id' => $userpagedata['id'],
+			'name' => $userpagedata['name'],
+			'userpagedata' => $userpagedata,
+			'customProfile' => $customProfile,
+		]);
+		die();
+	}
+	if (isset($_POST['post_comment'])) {
+		query("INSERT INTO channel_comments (id, comment, author, date, deleted) VALUES (?,?,?,?,?)",
+		[$userpagedata['id'],$_POST['comment'],$userdata['id'],time(),0]);
+		$message = "Channel comment has been submitted!";
+	}
+}
+
 if (!isset($userpagedata) || !$userpagedata) {
 	error('404', 'No user specified');
 }
 
-$page = (isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0 ? $_GET['page'] : 1);
+$pagination = (isset($_GET['p']) && is_numeric($_GET['p']) && $_GET['p'] > 0 ? $_GET['p'] : 1);
 $forceuser = isset($_GET['forceuser']);
 
-$limit = sprintf("LIMIT %s,%s", (($page - 1) * $lpp), $lpp);
+$limit = sprintf("LIMIT %s,%s", (($pagination - 1) * $lpp), $lpp);
 $latestVideoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, (SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views, v.author, v.tags FROM videos v JOIN users u ON v.author = u.id WHERE v.author = ? ORDER BY v.id DESC LIMIT 6", [$userpagedata['id']]);
 $count = result("SELECT COUNT(*) FROM videos l WHERE l.author = ?", [$userpagedata['id']]);
+
+$commentData = query("SELECT $userfields c.comment_id, c.id, c.comment, c.author, c.date, c.deleted FROM channel_comments c JOIN users u ON c.author = u.id WHERE c.id = ? ORDER BY c.date DESC", [$userpagedata['id']]);
 
 // Personal user page stuff
 if (isset($userdata['id']) && $userdata['id'] == $userpagedata['id'] && !$forceuser) {
@@ -116,12 +141,14 @@ echo $twig->render('user.twig', [
 	'userpagedata' => $userpagedata,
 	'latestVideos' => $latestVideoData,
 	'forceuser' => $forceuser,
-	'page' => $page,
+	'page' => $pagination,
 	'level_count' => $count,
 	'notifs' => (isset($notifications) ? $notifications : []),
 	'markread' => (isset($_GET['markread']) ? true : false),
 	'edited' => (isset($_GET['edited']) ? true : false),
 	'comments' => (isset($comments) ? $comments : null),
 	'subCount' => $subCount,
-	'customProfile' => $customProfile
+	'customProfile' => $customProfile,
+	'comments' => $commentData,
+	'message' => $message
 ]);
