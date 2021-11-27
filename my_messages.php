85599d469a34505f2a78f2b59a94a476014577f5
Gamerappa <chazpelo2@gmail.com>
2021-11-27 17:07:24 -0500

    port forums to 2008

--- my_messages.php
+++ my_messages.php
@@ -10,20 +10,27 @@ while ($notifdata = $notifsdata->fetch())
     {
         case 1:
             $notifications[] = sprintf('%s commented on <a href="watch.php?id=%s">%s</a>.', userlink($notifdata, 'u_') , $notifdata['l_id'], $notifdata['l_title']);
+			$id = $notifdata['id'];
         break;
         case 2:
             $notifications[] = sprintf('%s commented on your <a href="user.php?id=%s&forceuser">user page</a>.', userlink($notifdata, 'u_') , $userdata['id']);
+			$id = $notifdata['id'];
         break;
         case 3:
             $notifications[] = sprintf('%s sent you a private message: <a href="forum/showprivate.php?id=%s">Read</a>', userlink($notifdata, 'u_') , $notifdata['level']);
+			$id = $notifdata['id'];
         break;
         case 11:
+            $notifications[] = sprintf('%s %s', userlink($notifdata, 'u_') , $notifdata['level']);
+			$id = $notifdata['id'];
+        break;
         case 12:
         case 13:
         case 14:
         case 15:
         case 16:
             $notifications[] = sprintf('%s mentioned you in a %s comment: <a href="%s.php?id=%s">Read</a>', userlink($notifdata, 'u_') , cmtNumToType($notifdata['type'] - 10) , cmtNumToType($notifdata['type'] - 10) , $notifdata['level']);
+			$id = $notifdata['id'];
         break;
     }
 }
@@ -31,4 +38,5 @@ while ($notifdata = $notifsdata->fetch())
 $twig = twigloader();
 echo $twig->render('inbox.twig', [
 	'notifs' => (isset($notifications) ? $notifications : []),
+	'id' => (isset($id) ? $id : []),
 ]);
