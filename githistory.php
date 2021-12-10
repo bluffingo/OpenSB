e3fae5e8e4e6e548efcf7aef8dec666d545520e3
Blue2k <88458384+blue2000k@users.noreply.github.com>
2021-12-10 16:20:44 -0500

    port githistory.php from poktube

---
+++ githistory.php
@@ -0,0 +1,56 @@
+<?php
+// this code comes from gamerappa's poktube, -bluey december 10 2021
+require('lib/common.php');
+
+// Load Last 50 Git Logs
+$git_history = [];
+$git_logs = [];
+exec("git log -n 50", $git_logs);
+
+// Parse Logs
+$last_hash = null;
+foreach ($git_logs as $line)
+{
+    // Clean Line
+    $line = trim($line);
+
+    // Proceed If There Are Any Lines
+    if (!empty($line))
+    {
+        // Commit
+        if (strpos($line, 'commit') === 0)
+        {
+            $hash = explode(' ', $line);
+            $hash = trim(end($hash));
+            $git_history[$hash] = [
+                'message' => ''
+            ];
+            $last_hash = $hash;
+        }
+
+        // Author
+        else if (strpos($line, 'Author') !== false) {
+            $author = explode(':', $line);
+            $author = trim(end($author));
+            $git_history[$last_hash]['author'] = $author;
+        }
+
+        // Date
+        else if (strpos($line, 'Date') !== false) {
+            $date = explode(':', $line, 2);
+            $date = trim(end($date));
+            $git_history[$last_hash]['date'] = date('F j, Y, g:i a', strtotime($date));
+        }
+
+        // Message
+        else {
+            $git_history[$last_hash]['message'] .= $line ." ";
+        }
+    }
+}
+
+$twig = twigloader();
+echo $twig->render('githistory.twig', [
+    'git' => $git_history,
+]);
+?>
