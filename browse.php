4dae03550b12ae373ffa8119527371ece89be5a8
Blue2k <88458384+blue2000k@users.noreply.github.com>
2021-12-15 20:50:57 -0500

    fix bug on profiles and uh idfk what else

--- browse.php
+++ browse.php
@@ -6,7 +6,7 @@ $page = (isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0 
 
 $where = ($type != 'all' ? "WHERE v.category_id = ".type_to_cat($type) : '');
 $limit = sprintf("LIMIT %s,%s", (($page - 1) * $lpp), $lpp);
-$videoData = query("SELECT $userfields v.video_id, v.title, v.description, (SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views, v.time, v.videolength, v.author, v.category_id FROM videos v JOIN users u ON v.author = u.id $where ORDER BY v.id DESC $limit");
+$videoData = query("SELECT $userfields v.video_id, v.title, v.description, (SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views, v.time, v.videolength, v.author, v.category_id FROM videos v JOIN users u ON v.author = u.id $where ORDER BY v.time DESC $limit");
 $count = result("SELECT COUNT(*) FROM videos v $where");
 
 $twig = twigloader();
