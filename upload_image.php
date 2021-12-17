0e4e899a196e4d430556c8e80f43473d0a531015
Blue2k <88458384+blue2000k@users.noreply.github.com>
2021-12-16 21:11:14 -0500

    # This is a combination of 4 commits. # This is the 1st commit message:
    FUCK
    
    # This is the commit message #2:
    
    Holy Shit
    
    # This is the commit message #3:
    
    Delete shiftleft-analysis.yml
    # This is the commit message #4:
    
    fucking christ git you're a retard

---
+++ upload_image.php
@@ -0,0 +1,43 @@
+<?php
+//this uploads the music, should switch to a better solution!
+require('lib/common.php');
+
+//TODO: make music IDs not use multiple underscores.
+
+$music_id = substr(base64_encode(md5(bin2hex(random_bytes(6)))), 0, 11); //you are never too sure how much randomness you need.
+$new = '';
+foreach(str_split($music_id) as $char){
+	switch (rand(0, 4)) {
+		case rand(0, 1):
+			$char = str_rot13($char);
+			break;
+		case rand(0, 2):
+			$char = '_';
+			break;
+		case rand(0, 3):
+			$char = mb_strtoupper($char);
+			break;
+		case rand(0, 4):
+			$char = '-';
+			break;
+	}
+	$new .= $char;
+}
+
+if (isset($_POST['upload']) and isset($userdata['name'])) {
+	$title = (isset($_POST['title']) ? $_POST['title'] : null);
+
+	$name       = $_FILES['fileToUpload']['name'];
+	$temp_name  = $_FILES['fileToUpload']['tmp_name'];  // gets video info and thumbnail info
+	$ext  = pathinfo( $_FILES['fileToUpload']['name'], PATHINFO_EXTENSION );
+	$target_file = 'image/'.$new.'.'.$ext;
+	if (move_uploaded_file($temp_name, $target_file)){
+		query("INSERT INTO image (image_id, title, author, time, file) VALUES (?,?,?,?,?)",
+			[$new,$title,$userdata['id'],time(),'image/'.$new.'.'.$ext]);
+
+		redirect('./view.php?i='.$new);
+	}
+}
+
+$twig = twigloader();
+echo $twig->render('upload_image.twig');
