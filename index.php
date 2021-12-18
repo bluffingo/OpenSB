a8805f71334dc7e5b571b2824b457727636a7f08
Blue2k <88458384+blue2000k@users.noreply.github.com>
2021-12-18 16:41:05 -0500

    attempt making 2012 player work and giving up afterwards because fuck you

--- index.php
+++ index.php
@@ -5,10 +5,18 @@ require('lib/common.php');
 
 
 // currently selects all uploaded videos, should turn it into all featured only
+if ($frontend == "2012") {
+$videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, (SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views, v.videolength, v.tags, v.category_id, v.author FROM videos v JOIN users u ON v.author = u.id ORDER BY v.time DESC LIMIT 10");
+} else {
 $videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, (SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views, v.videolength, v.tags, v.category_id, v.author FROM videos v JOIN users u ON v.author = u.id ORDER BY RAND() LIMIT 10");
+}
 $videoDataRecentlyViewed = query("SELECT $userfields v.video_id, v.title, v.description, v.time, (SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views, v.videolength, v.tags, category_id, v.author, v.most_recent_view FROM videos v JOIN users u ON v.author = u.id ORDER BY v.most_recent_view DESC LIMIT 4");
 $videoDataRight = query("SELECT $userfields v.video_id, v.title, v.description, v.time, (SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views, v.videolength, v.tags, v.category_id, v.author FROM videos v JOIN users u ON v.author = u.id ORDER BY v.id DESC LIMIT 3");
+if ($frontend == "2012") {
+$featuredVideoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, (SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views, v.videolength, v.tags, category_id, v.author FROM videos v JOIN users u ON v.author = u.id ORDER BY RAND() DESC LIMIT 5"); //i have no clue how should flags even work.
+} else {
 $featuredVideoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, (SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views, v.videolength, v.tags, category_id, v.author FROM videos v JOIN users u ON v.author = u.id ORDER BY RAND() DESC LIMIT 1"); //i have no clue how should flags even work.
+}
 if ($log) {
 	$query = implode(', ', array_column(fetchArray(query("SELECT user FROM subscriptions WHERE id = ?", [$userdata['id']])), 'user'));
 	if($query != null) {
