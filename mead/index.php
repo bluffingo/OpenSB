c9701a12e62b5f45d0f85478da9a0e70c52a5076
Gamerappa <chazpelo2@gmail.com>
2022-01-30 15:33:20 -0500

    Update index.php

--- mead/index.php
+++ mead/index.php
@@ -7,5 +7,5 @@ $randomImage = $images[array_rand($images)]; // See comments
 ?>
 
 <img src="<?php echo $randomImage ?>" style="height:100%;margin-left: auto;margin-right: auto;display:block;">
-<center><h1>squareBracket Mead by Bluey2000k</h1>
+<center><h1>cheeseRox Mead by Gamerappa</h1>
 </center>
