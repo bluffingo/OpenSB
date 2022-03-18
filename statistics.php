aea8d2e8bd8b6db7827e55554090c39096b48def
forkiesassds <icanttellyou@memeware.net>
2022-03-18 18:35:16 +0200

    whoops

--- statistics.php
+++ statistics.php
@@ -1,7 +1,7 @@
 <?php
 require('lib/common.php');
 
-$stats = fetch("SELECT (SELECT COUNT(*) FROM users) usercount, (SELECT COUNT(*) FROM videos) videocount, (SELECT COUNT(*) FROM music) musiccount, (SELECT COUNT(*) FROM image) imagecount");
+$stats = fetch("SELECT (SELECT COUNT(*) FROM users) usercount, (SELECT COUNT(*) FROM videos) videocount");
 
 $twig = twigloader();
 echo $twig->render('stats.twig', [
