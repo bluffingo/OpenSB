fa27fea706db81ba32be9f34038db9c4b907d7d7
Chaz "Gamerappa" Péloquin <chazpelo2@gmail.com>
2022-06-28 16:40:37 -0400

    Update video.php

--- lib/video.php
+++ lib/video.php
@@ -1,78 +1,83 @@
 <?php
 namespace squareBracket;
 // Functions related to sbNext Video Stuff.
-
-static function categoryIDToName($id): string
+static function categoryIDToName($id)
+{
+    switch ($id)
     {
-        switch ($id) {
-            case 0:
-                $name = __('Miscellanous');
-                break;
-            case 1:
-                $name = __('Entertainment');
-                break;
-            case 2:
-                $name = __('Comedy & Humor');
-                break;
-            case 3:
-                $name = __('Gaming');
-                break;
-            case 4:
-                $name = __('News and Updates');
-                break;
-            case 5:
-                $name = __('Life');
-                break;
-            case 6:
-                $name = __('Science & Technology');
-                break;
-            case 7:
-                $name = __('Archive Dump');
-                break;
-        }
-        return $name;
+        case 0:
+            $name = __('Miscellanous');
+        break;
+        case 1:
+            $name = __('Entertainment');
+        break;
+        case 2:
+            $name = __('Comedy & Humor');
+        break;
+        case 3:
+            $name = __('Gaming');
+        break;
+        case 4:
+            $name = __('News and Updates');
+        break;
+        case 5:
+            $name = __('Life');
+        break;
+        case 6:
+            $name = __('Science & Technology');
+        break;
+        case 7:
+            $name = __('Archive Dump');
+        break;
+    }
+    return $name;
 }
 
-static function type_to_cat($type): int
+static function type_to_cat($type)
+{
+    switch ($type)
     {
-        switch ($type) {
-            case 'misc':
-                $cat = 0;
-                break;
-            case 'entertainment':
-                $cat = 1;
-                break;
-            case 'comedy':
-                $cat = 2;
-                break;
-            case 'gaming':
-                $cat = 3;
-                break;
-            case 'news':
-                $cat = 4;
-                break;
-            case 'life':
-                $cat = 5;
-                break;
-            case 'technology':
-                $cat = 6;
-                break;
-            case 'backup':
-                $cat = 7;
-                break;
-        }
-        return $cat;
+        case 'misc':
+            $cat = 0;
+        break;
+        case 'entertainment':
+            $cat = 1;
+        break;
+        case 'comedy':
+            $cat = 2;
+        break;
+        case 'gaming':
+            $cat = 3;
+        break;
+        case 'news':
+            $cat = 4;
+        break;
+        case 'life':
+            $cat = 5;
+        break;
+        case 'technology':
+            $cat = 6;
+        break;
+        case 'backup':
+            $cat = 7;
+        break;
+    }
+    return $cat;
 }
 
 // This is for the like-to-dislike ratio lightsaber. -Gamerappa, November 2nd 2021
-function calculateRatio($number, $percent, $total){
-	// If there's no ratio or dislikes, it returns 100.
-	if ($total == 0 or $number == 0) {
-		return 100;
-	} else {
-	// It returns the Like-to-dislike ratio.
-    return ($percent / $total) * $number * 100;
-	}
+function calculateRatio($number, $percent, $total)
+{
+    // If there's no ratio or dislikes, it returns 100.
+    if ($total == 0 or $number == 0)
+    {
+        return 100;
+    }
+    else
+    {
+        // It returns the Like-to-dislike ratio.
+        return ($percent / $total) * $number * 100;
+    }
 }
 
 /**
@@ -80,14 +85,16 @@ function calculateRatio($number, $percent, $total){
  *
  * @return string String to put inside a SQL statement.
  */
-function videofields() {
+function videofields()
+{
     $fields = ['video_id', 'title', 'description', 'time', "author", "videolength", "tags"];
-    
+
     $out = '';
-    foreach ($fields as $field) {
+    foreach ($fields as $field)
+    {
         $out .= sprintf('v.%s,', $field, $field);
     }
-    
+
     $out .= "(SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views";
     return $out;
 }
