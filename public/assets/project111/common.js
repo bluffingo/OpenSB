93eeb5b4b4a92972f7faecc4882e070e7f3b4372
Chaz "Gamerappa" Peloquin <chazpelo2@gmail.com>
2022-08-19 15:17:20 -0400

    unimplemented warning for 111
    also done with vanilla js, yes.

--- public/assets/project111/common.js
+++ public/assets/project111/common.js
@@ -1,3 +1,17 @@
-$(document).ready(function() {
-  // Add javascript specific to Project 111.
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("sb 111 js loaded");
+
+    btns = document.getElementsByClassName("sb-unimplemented");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            Bulma().alert({
+                type: 'info',
+                title: 'This feature is not implemented',
+                body: '111 is incomplete. For a complete experience of squareBracket, Finalium should be used for ' +
+                    'the time being.',
+                confirm: 'OK',
+            });
+        });
+    }
+
 });
