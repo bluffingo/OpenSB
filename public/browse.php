d97c9e52bc4c14ebbd6926bd6629c2243ea6c528
Chaz "Gamerappa" Peloquin <chazpelo2@gmail.com>
2022-08-24 13:56:48 -0400

    tweak finalium scss
    remove video time css code since it was stolen off a youtube stylesheet.

--- public/browse.php
+++ public/browse.php
@@ -7,7 +7,7 @@ require dirname(__DIR__) . '/private/class/common.php';
 $type = ($_GET['type'] ?? 'all');
 $page = (isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0 ? $_GET['page'] : 1);
 
-$where = ($type != 'all' ? "WHERE v.category_id = " . type_to_cat($type) : '');
+$where = ($type != 'all' ? "WHERE v.category_id = " . Videos::type_to_cat($type) : '');
 $limit = sprintf("LIMIT %s,%s", (($page - 1) * $paginationLimit), $paginationLimit);
 $videoData = $sql->query("SELECT $userfields $videofields FROM videos v JOIN users u ON v.author = u.id $where ORDER BY v.time DESC $limit");
 $count = $sql->result("SELECT COUNT(*) FROM videos v $where");
