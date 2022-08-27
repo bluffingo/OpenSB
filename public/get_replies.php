3d3f77f1d08707745ed11f1018e822a7db75dff8
Chaz "Gamerappa" Peloquin <chazpelo2@gmail.com>
2022-08-27 19:14:57 -0400

    Update get_replies.php

--- public/get_replies.php
+++ public/get_replies.php
@@ -7,7 +7,7 @@ if (!isset($_POST['comment_id'])) {
     die();
 }
 
-$commentData = query("SELECT $userfields c.comment_id, c.id, c.comment, c.author, c.date, c.deleted, c.reply_to FROM comments c JOIN users u ON c.author = u.id WHERE c.reply_to = ? ORDER BY c.date DESC", [$_POST['comment_id']]);
+$commentData = $sql->query("SELECT $userfields c.comment_id, c.id, c.comment, c.author, c.date, c.deleted, c.reply_to FROM comments c JOIN users u ON c.author = u.id WHERE c.reply_to = ? ORDER BY c.date DESC", [$_POST['comment_id']]);
 
 $twig = twigloader();
 $template = $twig->createTemplate('{% for comment in comments %}{{ comment(comment) }}{% endfor %}');
