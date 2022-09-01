3771f5655bd6d73590e90ced969ae3dd0d97fd46
Chaz "Gamerappa" Peloquin <chazpelo2@gmail.com>
2022-09-01 15:39:45 -0400

    new watch page design for nostalgia theme
    incomplete but whatever

--- public/ajax_watch.php
+++ public/ajax_watch.php
@@ -8,7 +8,7 @@ if (isset($_POST['limit'])) {
     $offset = (isset($_POST['from']) ? $_POST['from'] : 0);
     $user = (isset($_POST['user']) ? $_POST['user'] : 0);
 
-    $videoData = query("SELECT $userfields $videofields FROM videos v JOIN users u ON v.author = u.id ORDER BY v.id DESC LIMIT ? OFFSET ?", [$limit, $offset]);
+    $videoData = $sql->query("SELECT $userfields $videofields FROM videos v JOIN users u ON v.author = u.id ORDER BY v.id DESC LIMIT ? OFFSET ?", [$limit, $offset]);
 
     $twig = twigloader();
     echo $twig->render('components/videolist.twig', [
