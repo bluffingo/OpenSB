a1379e1c94852e2867b874d49a1ef444096d6234
Chaz "Gamerappa" Peloquin <chazpelo2@gmail.com>
2022-09-03 12:30:12 -0400

    fix cattledog support

--- private/lang/language_names.php
+++ private/lang/language_names.php
@@ -4,16 +4,18 @@ namespace squareBracket;
 
 use WhiteCube\Lingua\Service as Lingua;
 
-$dir = new \DirectoryIterator('../private/lang/');
-foreach ($dir as $file) {
-    if ($file->getFilename() != "template.json" and $file->getFilename() != "qps-plocm.json") {
-        if ($file->getExtension() == "json") {
-            $language = Lingua::createFromW3C($file->getBasename('.json'));
-            $languages[$file->getBasename('.json')] = $language->toName();
-        }
-    }
-}
+if (!isCattleDog()) {
+	$dir = new \DirectoryIterator('../private/lang/');
+	foreach ($dir as $file) {
+		if ($file->getFilename() != "template.json" and $file->getFilename() != "qps-plocm.json") {
+			if ($file->getExtension() == "json") {
+				$language = Lingua::createFromW3C($file->getBasename('.json'));
+				$languages[$file->getBasename('.json')] = $language->toName();
+			}
+		}
+	}
 
-if ($isDebug) {
-    $languages['qps-plocm'] = "Pseudolocalization";
+	if ($isDebug) {
+		$languages['qps-plocm'] = "Pseudolocalization";
+	}
 }
