57c734159a70d8d50d1738e58b7fdadf6295b36e
Chaz "Gamerappa" Peloquin <chazpelo2@gmail.com>
2022-09-16 18:16:25 -0400

    have 16 columns instead of 12
    and now we have more precise sizing while still using the dogshit port of the bootstrap 3 grid system.

--- public/statistics.php
+++ public/statistics.php
@@ -4,7 +4,7 @@ namespace squareBracket;
 
 require dirname(__DIR__) . '/private/class/common.php';
 
-$stats = $sql->fetch("SELECT (SELECT COUNT(*) FROM users) usercount, (SELECT COUNT(*) FROM videos) videocount, (SELECT COUNT(*) FROM views) viewcount");
+$stats = $sql->fetch("SELECT (SELECT COUNT(*) FROM users) usercount, (SELECT COUNT(*) FROM videos) videocount, (SELECT COUNT(*) FROM views) viewcount, (SELECT COUNT(*) FROM comments) commentcount");
 
 $twig = twigloader();
 echo $twig->render('stats.twig', [
