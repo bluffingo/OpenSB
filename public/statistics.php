a9885f70227f8b3942698b56cdb1076d8e44e56a
Chaz "Gamerappa" Peloquin <chazpelo2@gmail.com>
2022-09-24 16:03:42 -0400

    add graph on stats page

--- public/statistics.php
+++ public/statistics.php
@@ -6,7 +6,12 @@ require dirname(__DIR__) . '/private/class/common.php';
 
 $stats = $sql->fetch("SELECT (SELECT COUNT(*) FROM users) usercount, (SELECT COUNT(*) FROM videos) videocount, (SELECT COUNT(*) FROM views) viewcount, (SELECT COUNT(*) FROM comments) commentcount");
 
+$vidGraph = GraphData::getVideoGraph();
+$userGraph = GraphData::getUserGraph();
+
 $twig = twigloader();
 echo $twig->render('stats.twig', [
-    'stats' => $stats
+    'stats' => $stats,
+    'video_graph_data' => $vidGraph,
+    'user_graph_data' => $userGraph,
 ]);
