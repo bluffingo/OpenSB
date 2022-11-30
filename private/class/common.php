009e636ccd275c68adce62cee3309e51435eddb1
Chaz "Gamerappa" Péloquin <chazpelo2@gmail.com>
2022-11-30 15:26:23 -0500

    Update common.php

--- private/class/common.php
+++ private/class/common.php
@@ -58,7 +58,7 @@ if (!isset($_SESSION['isCattleDog'])) {
     $videofields = "v.id, v.video_id, v.title, v.description, v.time, v.post_type, (SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views, (SELECT COUNT(*) FROM comments WHERE id = v.video_id) AS comments, (SELECT COUNT(*) FROM favorites WHERE video_id = v.video_id) AS favorites, (SELECT COUNT(*) FROM favorites WHERE video_id = v.video_id) AS favorites, v.videolength, v.category_id, v.author";
 }
 
-if (preg_match('~MSIE|Internet Explorer~i', $_SERVER['HTTP_USER_AGENT']) || preg_match('~Trident/7.0(x*)?; rv:11.0~',$_SERVER['HTTP_USER_AGENT'])) {
+if (preg_match('~MSIE|Internet Explorer~i', $_SERVER['HTTP_USER_AGENT']) || preg_match('~Trident/7.0(/x*/)?; rv:11.0~',$_SERVER['HTTP_USER_AGENT'])) {
 	$browser['legacy_masthead_fix'] = true;
 	$browser['legacy_disable_graph'] = true;
 	if (preg_match('/MSIE (.*?);/', $_SERVER['HTTP_USER_AGENT'])) {
