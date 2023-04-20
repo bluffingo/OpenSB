9c21e232f9a5290a49a458dff933e73b21acb26c
Chaziz <chazpelo2@gmail.com>
2023-04-20 19:04:11 -0400

    Update hash-emails.php

--- tools/hash-emails.php
+++ tools/hash-emails.php
@@ -1,19 +1,18 @@
 #!/usr/bin/php
 <?php
+namespace openSB;
 
 print("principia-web tools - hash-emails.php\n");
 print("=========================================\n");
 print("Hashes emails in a database with unhashed emails.\n");
 
-namespace openSB;
-
 require_once dirname(__DIR__) . '/private/class/common.php';
 
-$users = query("SELECT id, email FROM users");
+$users = $sql->query("SELECT id, email FROM users");
 
 while ($user = $users->fetch()) {
 	$id = $user['id'];
 	$emailhash = mailhash($user['email']);
 
-	query("UPDATE users SET email = ? WHERE id = ?", [$emailhash, $id]);
+	$sql->query("UPDATE users SET email = ? WHERE id = ?", [$emailhash, $id]);
 }
