23718ccfd677d822eec4a344133103b96e86ca1f
Chaziz <chazpelo2@gmail.com>
2023-05-27 17:10:04 -0400

    uh wtf

--- tools/compile-scss-dartsass-prod.sh
+++ tools/compile-scss-dartsass-prod.sh
@@ -2,4 +2,6 @@
 
 common_arguments="--style expanded --no-source-map --load-path ./"
 
+# did git forget to pull this to prod??
+
 sass ${common_arguments} finalium/assets/stylesheets/:public/assets/css/
--- tools/compile-scss-dartsass.sh
+++ tools/compile-scss-dartsass.sh
@@ -4,4 +4,6 @@
 # Load path is assuming you're running this script from the root of the sB site directory
 common_arguments="--style compressed --no-source-map --load-path ./ --watch"
 
+# did git forget to pull this to prod??
+
 sass ${common_arguments} finalium/assets/stylesheets/:public/assets/css/
