fe012eb5c274efcfa33984ce0b8d2b59208ebe05
Chaziz <chazpelo2@gmail.com>
2023-06-10 17:50:57 -0400

    metadata tags test

--- betty/class/Templating.php
+++ betty/class/Templating.php
@@ -45,6 +45,9 @@ class Templating
         $this->twig->addGlobal('skins', $this->getAllSkinsMetadata());
         $this->twig->addGlobal('session', $_SESSION);
         $this->twig->addGlobal('website_branding', $branding);
+
+        $this->twig->addGlobal("page_url", (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+        $this->twig->addGlobal("domain", (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/");
     }
 
     /**
