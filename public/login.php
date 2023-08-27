30914354d834c11a2c6bb69dbfdec0bf5bf5f9e1
Katie Péloquin <chazpelo2@gmail.com>
2023-08-27 13:37:12 -0400

    register page and dumbass permission fuckery

--- public/login.php
+++ public/login.php
@@ -13,32 +13,6 @@ $page = new \Orange\Pages\AccountLogin($betty);
 
 if (isset($_POST["loginsubmit"])) {
     $page->postData($_POST);
-    $username = ($_POST['username'] ?? null);
-    $password = ($_POST['password'] ?? null);
-
-    // Check to see if the user actually has entered anything.
-    if (!$username) $error = __("Please enter your username! ");
-    if (!$password) $error = __("Please enter your password! ");
-
-    if (empty($error)) {
-        $logindata = $sql->fetch("SELECT password,token FROM users WHERE name = ?", [$username]);
-        if ($logindata && password_verify($password, $logindata['password'])) {
-            setcookie('SBTOKEN', $logindata['token'], 2147483647);
-            $nid = $sql->result("SELECT id FROM users WHERE token = ?", [$logindata['token']]);
-            $sql->query("UPDATE users SET lastview = ?, ip = ? WHERE id = ?", [time(), getUserIpAddr(), $nid]);
-
-            if ($googleAPI) {
-                $loginEventData = new LoginEvent();
-                $loginEventData->setMethod('BettySB');
-                $baseRequest->addEvent($loginEventData);
-                $ga->send($baseRequest);
-            }
-
-            redirect('./');
-        } else {
-            $error = __("Incorrect username or password.");
-        }
-    }
 }
 
 $twig = new \Orange\Templating($betty);
--- public/register.php
+++ public/register.php
@@ -4,61 +4,15 @@ namespace openSB;
 
 global $gump, $betty;
 
-use Br33f\Ga4\MeasurementProtocol\Dto\Event\SignUpEvent;
-
 require_once dirname(__DIR__) . '/private/class/common.php';
 
-$error = '';
+require_once dirname(__DIR__) . '/orange/classes/Pages/AccountRegister.php';
+
+$page = new \Orange\Pages\AccountRegister($betty);
 
 if (isset($_POST['registersubmit']) or isset($_POST['terms_agreed'])) {
-
-    $ipcheck = file_get_contents("http://api.stopforumspam.org/api?ip=" . getUserIpAddr());
-
-    if (!str_contains($ipcheck, "<appears>yes</appears>")) {
-        error(403, "Your IP looks suspicious.");
-        //$orange->Notification("Your IP looks suspicious.", "/register.php");
-        //exit();
-    }
-
-    $gump->validation_rules([
-        "username" => "required|alpha_numeric|max_len,128|min_len,1",
-        "pass1"    => "required|max_len,128|min_len,8",
-        "pass2"    => "required|equalsfield,pass1",
-        "email"    => "required|valid_email|max_len,128",
-    ]);
-
-    $gump->filter_rules([
-        "username" => "trim",
-        "pass1"    => "trim",
-        "pass2"    => "trim",
-        "email"    => "trim|sanitize_email",
-    ]);
-
-    $filter = $gump->run($_POST);
-
-    if ($gump->errors()) {
-        $error = $gump->get_errors_array();
-    } else {
-        $username = (string)$filter['username'];
-        $pass = $filter['pass1'];
-        $pass2 = $filter['pass2'];
-        $mail = (string)filter_var($filter['email'], FILTER_SANITIZE_EMAIL);
-
-        if ($sql->result("SELECT COUNT(*) FROM users WHERE name = ?", [$username])) $error .= __("Username has already been taken. "); //ashley2012 bypassed this -gr 7/26/2021
-        if (!preg_match('/^[a-zA-Z0-9\-_]+$/', $username)) $error .= __("Username contains invalid characters (Only alphanumeric and underscore allowed). "); //ashley2012 bypassed this with the long-ass arabic character. -gr 7/26/2021
-        if ($sql->result("SELECT COUNT(*) FROM users WHERE email = ?", [$mail])) $error .= "You've already registered an account using this email address. ";
-        if ($sql->result("SELECT COUNT(*) FROM users WHERE ip = ?", [getUserIpAddr()]) > 10)
-            $error .= "Creating more than 10 accounts isn't allowed. ";
-
-        $token = bin2hex(random_bytes(32));
-        $sql->query("INSERT INTO users (name, password, token, joined, title, email) VALUES (?,?,?,?,?,?)",
-            [$username, password_hash($pass, PASSWORD_DEFAULT), $token, time(), $username, mailHash($mail)]);
-
-        setcookie('SBTOKEN', $token, 2147483647);
-
-        redirect('./');
-    }
+    $page->postData($_POST);
 }
 
-$twig = twigloader();
-echo $twig->render('register.twig', ['errors' => $error]);
+$twig = new \Orange\Templating($betty);
+echo $twig->render('register.twig');
