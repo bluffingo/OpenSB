40c667e75f5486733f08ad30ffa6a46fbc5cad80
Katie Péloquin <chazpelo2@gmail.com>
2023-10-18 18:28:07 -0400

    Also fix logging out

--- public/logout.php
+++ public/logout.php
@@ -2,7 +2,9 @@
 
 namespace openSB;
 
+global $betty;
+
 require_once dirname(__DIR__) . '/private/class/common.php';
 //TODO: Add "successfully logged out" message when you log out. -gr 7/26/2021
 setcookie("SBTOKEN", "", time() - 3600);
-redirect('./'); //TODO: Redirect to login page when message is implemented -gr 7/26/2021
+$betty->Notification("Logged out!", "/", "success");
