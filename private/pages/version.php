817f7c3e64a179b5487155d07a0379bcd898a5eb
Bluffingo <chazpelo2@gmail.com>
2024-06-04 17:12:57 -0400

    merge version page

--- private/pages/version.php
+++ private/pages/version.php
@@ -2,13 +2,53 @@
 
 namespace OpenSB;
 
-global $twig, $orange;
+global $twig, $database;
 
-use SquareBracket\Pages\Version;
-use SquareBracket\Templating;
+use Composer\ComposerInstalled;
 
-$page = new Version($orange);
+/**
+ * Get a list of all the installed Composer packages. Akin to getExternalLibraries on MediaWiki.
+ *
+ * @since SquareBracket 1.1
+ *
+ * @return array
+ */
+function getComposerPackages(): array
+{
+    $dependencies = [];
+
+    $installed = new ComposerInstalled(SB_VENDOR_PATH . '/composer/installed.json');
+
+    $dependencies += $installed->getInstalledDependencies();
+
+    ksort($dependencies);
+
+    return $dependencies;
+}
+
+$data = [
+    "developers" => [
+        'Chaziz',
+        'icanttellyou',
+        'ROllerozxa',
+    ],
+    "software" => [
+        'orangeVersion' => [
+            'title' => "OpenSB",
+            'info' => (new \Core\VersionNumber)->getVersionString(),
+        ],
+        'phpVersion' => [
+            'title' => "PHP",
+            'info' => phpversion(),
+        ],
+        'dbVersion' => [
+            'title' => "Database software",
+            'info' => $database->getVersion(),
+        ],
+    ],
+    "composer_packages" => getComposerPackages(),
+];
 
 echo $twig->render('version.twig', [
-    'data' => $page->getData(),
+    'data' => $data,
 ]);
