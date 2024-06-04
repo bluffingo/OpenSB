9d0bcde6855fe9dc22109f13c2a7f7c1623c878b
Bluffingo <chazpelo2@gmail.com>
2024-06-04 16:32:30 -0400

    do same for index and login + implement "remember me"

--- private/pages/index.php
+++ private/pages/index.php
@@ -2,13 +2,21 @@
 
 namespace OpenSB;
 
-global $twig, $orange;
+global $twig, $database;
 
-use SquareBracket\Pages\Index;
-use SquareBracket\Templating;
+use SquareBracket\UnorganizedFunctions;
 
-$index = new Index($orange);
-$data = $index->getData();
+$whereRatings = UnorganizedFunctions::whereRatings();
+
+$submissions = $database->fetchArray($database->query("SELECT v.* FROM videos v WHERE v.video_id NOT IN (SELECT submission FROM takedowns) AND $whereRatings ORDER BY RAND() LIMIT 24"));
+$submissions_recent = $database->fetchArray($database->query("SELECT v.* FROM videos v WHERE v.video_id NOT IN (SELECT submission FROM takedowns) AND $whereRatings ORDER BY v.time DESC LIMIT 24"));
+$news_recent = $database->fetchArray($database->query("SELECT j.* FROM journals j WHERE j.is_site_news = 1 ORDER BY j.date DESC LIMIT 3"));
+
+$data = [
+    "submissions" => UnorganizedFunctions::makeSubmissionArray($database, $submissions),
+    "submissions_new" => UnorganizedFunctions::makeSubmissionArray($database, $submissions_recent),
+    "news_recent" => $news_recent,
+];
 
 echo $twig->render('index.twig', [
     'data' => $data,
--- private/pages/login.php
+++ private/pages/login.php
@@ -2,24 +2,57 @@
 
 namespace OpenSB;
 
-global $twig, $orange;
+global $twig, $database;
 
-use SquareBracket\Pages\AccountLogin;
-use SquareBracket\Templating;
-
-$error = '';
-
-$page = new AccountLogin($orange);
+use Core\Utilities;
+use SquareBracket\UnorganizedFunctions;
 
 if (isset($_POST["loginsubmit"])) {
-    $page->postData($_POST);
+    $error = false;
+
+    $username = ($_POST['username'] ?? null);
+    $password = ($_POST['password'] ?? null);
+    $rememberMe = isset($_POST['remember_me']);
+
+    if (!$username) $error = true;
+    if (!$password) $error = true;
+
+    if (!$error) {
+        $logindata = $database->fetch("SELECT password,token,ip FROM users WHERE name = ?", [$username]);
+
+        if ($logindata && password_verify($password, $logindata['password'])) {
+            // check if the account is from an ip that is in ipbans
+            $ipban = $database->fetch("SELECT * FROM ipbans WHERE ? LIKE ip", [$logindata['ip']]);
+
+            if ($ipban) {
+                UnorganizedFunctions::Notification("This account's latest IP address is banned.", "/login.php");
+            }
+
+            if ($rememberMe) {
+                $expires = time() + (365 * 24 * 60 * 60);
+            } else {
+                $expires = time() + (30 * 24 * 60 * 60);
+            }
+
+            setcookie('SBTOKEN', $logindata['token'], [
+                'expires' => $expires,
+                'path' => '/',
+                'domain' => '',
+                'secure' => true,
+                'httponly' => true,
+                'samesite' =>'Lax',
+            ]);
+
+            $nid = $database->result("SELECT id FROM users WHERE token = ?", [$logindata['token']]);
+            $database->query("UPDATE users SET lastview = ?, ip = ? WHERE id = ?", [time(), Utilities::get_ip_address(), $nid]);
+
+            UnorganizedFunctions::redirect('./');
+        } else {
+            UnorganizedFunctions::Notification("Incorrect credentials.", "/login.php");
+        }
+    } else {
+        UnorganizedFunctions::Notification("Please input your credentials.", "/login.php");
+    }
 }
 
-
-
-echo $twig->render('login.twig', [
-    'error' => $error,
-    'resetted' => isset($_GET['resetted']),
-    'new_pass' => isset($_GET['new_pass']),
-    'new_token' => isset($_GET['new_token']),
-]);
+echo $twig->render('login.twig');
