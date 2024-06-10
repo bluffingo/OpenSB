97c2ad1186d7b3cd6c0b05e35f4cd2b3a711c3af
Bluffingo <chazpelo2@gmail.com>
2024-06-09 21:57:10 -0400

    virtue/arson shit

--- private/class/SquareBracket/Templating.php
+++ private/class/SquareBracket/Templating.php
@@ -68,10 +68,6 @@ class Templating
         $this->twig->addExtension(new StringExtension());
 
         // 2021 SQUAREBRACKET FRONTEND COMPATIBILITY
-        $this->twig->addFunction(new TwigFunction('__', function($string, $placeholders = []) {
-            return vsprintf($string, $placeholders);
-        }));
-
         $this->twig->addFunction(new TwigFunction('small_video_box', function() {
             return false;
         }));
