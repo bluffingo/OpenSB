dac03702b846d76b921a41b1f7b5992daadb7f0d
kcielz <106561594+kcielz@users.noreply.github.com>
2024-06-11 18:53:12 -0400

    Fix deprecated shit (This hasnt been changed in the SLIGHTEST way 💀)
    Hi! -luna

--- tools/compile-scss.sh
+++ tools/compile-scss.sh
@@ -3,21 +3,27 @@
 command=$1
 
 if [ "$command" = "dev" ]; then
-common_arguments="--style expanded --no-source-map --load-path ./ --watch"
+    common_arguments="--style expanded --sourcemap=none --update"
 else
-common_arguments="--style expanded --no-source-map --load-path ./"
+    common_arguments="--style expanded --sourcemap=none"
 fi
 
 machine=$(uname -s)
 case "${machine}" in
-CYGWIN*|MINGW*|MSYS*)   machine=Windows;;
-*)                      machine=Other;;
+    CYGWIN*|MINGW*|MSYS*)   machine=Windows;;
+    *)                      machine=Other;;
 esac
 
 if [ "$machine" == "Windows" ]; then
-sass_executable="sass.bat"
+    sass_executable="sass.bat"
 else
-sass_executable="sass"
+    sass_executable="sass"
 fi
 
-${sass_executable} ${common_arguments} scss/bootstrap/:public/assets/css/ scss/finalium/:public/assets/css/ scss/biscuit/:public/assets/css/
+
+directories=("scss/bootstrap/:public/assets/css/" "scss/finalium/:public/assets/css/" "scss/biscuit/:public/assets/css/")
+
+
+for directory in "${directories[@]}"; do
+    ${sass_executable} ${common_arguments} $directory
+done
