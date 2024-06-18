c155e1c0a03a623186c12aea2733e2c4fe3ec5f8
Bluffingo <chazpelo2@gmail.com>
2024-06-17 23:20:15 -0400

    update scripts

--- tools/2024-02-rename_downloaded_vids.php
+++ tools/2024-02-rename_downloaded_vids.php
@@ -1,15 +1,13 @@
 <?php
 namespace OpenSB;
 
-global $orange;
+global $database;
 define("SB_DYNAMIC_PATH", dirname(__DIR__) . '/dynamic');
 define("SB_PRIVATE_PATH", dirname(__DIR__) . '/private');
 define("SB_VENDOR_PATH", dirname(__DIR__) . '/vendor');
 define("SB_GIT_PATH", dirname(__DIR__) . '/.git'); // ONLY FOR makeVersionString() IN SquareBracket CLASS.
 
 require_once dirname(__DIR__) . '/private/class/common.php';
-
-$sql = $orange->getDatabase();
 
 $videos = SB_DYNAMIC_PATH . '/videos';
 
@@ -19,7 +17,7 @@ foreach ($files as $file) {
 	$full_path = $videos . '/' . $file;
 	$bunny_guid = substr($file, 34, -18);
 	
-	$id = $sql->result("SELECT v.video_id FROM videos v WHERE v.videofile = ?", [$bunny_guid]);
+	$id = $database->result("SELECT v.video_id FROM videos v WHERE v.videofile = ?", [$bunny_guid]);
 	
 	$new_full_path = $videos . '/' . $id . '.converted.mp4';
 	
--- tools/2024-02-video_url_export.php
+++ tools/2024-02-video_url_export.php
@@ -1,7 +1,7 @@
 <?php
 namespace OpenSB;
 
-global $orange;
+global $database;
 define("SB_DYNAMIC_PATH", dirname(__DIR__) . '/dynamic');
 define("SB_PRIVATE_PATH", dirname(__DIR__) . '/private');
 define("SB_VENDOR_PATH", dirname(__DIR__) . '/vendor');
@@ -9,18 +9,13 @@ define("SB_GIT_PATH", dirname(__DIR__) . '/.git'); // ONLY FOR makeVersionString
 
 require_once dirname(__DIR__) . '/private/class/common.php';
 
-$sql = $orange->getDatabase();
+// 1708565417 is February 2024 (MCwETGfmUDu). It and everything posted beforehand has already been archived.
+$videoData = $database->fetchArray($database->query("SELECT v.* FROM videos v WHERE v.post_type = 0 AND v.time > 1708565417 ORDER BY v.time DESC"));
 
-// 1669421577 is November 2022, during the "transitional" period between squareBracket.pw and Qobo.tv.
-// Anything uploaded after that date only exists inside of the BunnyCDN shit Qobo was using.
-// Anything before then has higher quality versions from squareBracket.pw.
-$videoData = $sql->fetchArray($sql->query("SELECT v.* FROM videos v WHERE v.post_type = 0 AND v.time > 1669421577 ORDER BY v.time DESC"));
+$file = fopen(dirname(__DIR__) . "/tools/video_urls.txt", "w") or die("Can't open.");
 
-$file = fopen("video_urls.txt", "w") or die("Can't open.");
-
-// note to anyone: this bunnycdn video cdn domain no longer works.
 foreach ($videoData as $video) {
-	$name = "https://vz-05de22db-96d.b-cdn.net/" . $video["videofile"] . "/playlist.m3u8";
+	$name = "https://vz-1655021f-b55.b-cdn.net/" . $video["videofile"] . "/playlist.m3u8";
 	print($name . PHP_EOL);
 	fwrite($file, $name . PHP_EOL);
 }
