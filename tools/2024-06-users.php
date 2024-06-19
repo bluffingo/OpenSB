7811c65659e2b6f2c8941ecbab38f83e1d56d0da
Bluffingo <chazpelo2@gmail.com>
2024-06-19 02:27:27 -0400

    fix screwed up script

--- tools/2024-06-users.php
+++ tools/2024-06-users.php
@@ -14,9 +14,9 @@ require_once dirname(__DIR__) . '/private/class/common.php';
 
 $users = $database->fetchArray($database->query("SELECT id, name, joined FROM users ORDER BY joined ASC"));
 
-var_dump($users);
-
 $new_id = 0;
+$id_mapping = [];
+$temp_id_base = 1000000; // avoid stupid conflicting shit
 
 // internally update all user ids to be in an order more akin to join date.
 // squarebracket's prod db has a gap between id205 and id1654 due to a botting incident from around june 2023
@@ -29,32 +29,60 @@ $new_id = 0;
 // -chaziz 6/19/2024
 foreach ($users as $user) {
     $new_id++;
-    var_dump($new_id);
+    $temp_id = $temp_id_base + $new_id;
+    $id_mapping[$user["id"]] = $temp_id;
+    echo "{$user['id']} to $temp_id\n";
+}
 
-    // do this before running: ALTER TABLE `users` ADD `new_id` INT NULL DEFAULT NULL AFTER `id`;
-    $database->query("UPDATE bans SET userid = ? WHERE userid = ?", [$new_id, $user["id"]]);
-    $database->query("UPDATE channel_comments SET author = ? WHERE author = ?", [$new_id, $user["id"]]);
-    $database->query("UPDATE comments SET author = ? WHERE author = ?", [$new_id, $user["id"]]);
-    $database->query("UPDATE favorites SET user_id = ? WHERE user_id = ?", [$new_id, $user["id"]]);
-    $database->query("UPDATE invite_keys SET generated_by = ? WHERE generated_by = ?", [$new_id, $user["id"]]);
-    $database->query("UPDATE invite_keys SET claimed_by = ? WHERE claimed_by = ?", [$new_id, $user["id"]]);
-    $database->query("UPDATE journals SET author = ? WHERE author = ?", [$new_id, $user["id"]]);
-    $database->query("UPDATE journal_comments SET author = ? WHERE author = ?", [$new_id, $user["id"]]);
-    $database->query("UPDATE notifications SET recipient = ? WHERE recipient = ?", [$new_id, $user["id"]]);
-    $database->query("UPDATE notifications SET sender = ? WHERE sender = ?", [$new_id, $user["id"]]);
-    $database->query("UPDATE rating SET user = ? WHERE user = ?", [$new_id, $user["id"]]);
-    $database->query("UPDATE subscriptions SET id = ? WHERE id = ?", [$new_id, $user["id"]]);
-    $database->query("UPDATE subscriptions SET user = ? WHERE user = ?", [$new_id, $user["id"]]);
-    $database->query("UPDATE takedowns SET sender = ? WHERE sender = ?", [$new_id, $user["id"]]);
-    $database->query("UPDATE users SET new_id = ? WHERE id = ?", [$new_id, $user["id"]]);
-    $database->query("UPDATE user_old_names SET user = ? WHERE user = ?", [$new_id, $user["id"]]);
-    $database->query("UPDATE videos SET author = ? WHERE author = ?", [$new_id, $user["id"]]);
+var_dump($id_mapping);
+
+foreach ($id_mapping as $old_id => $temp_id) {
+    $database->query("UPDATE bans SET userid = ? WHERE userid = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE channel_comments SET id = ? WHERE id = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE channel_comments SET author = ? WHERE author = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE comments SET author = ? WHERE author = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE favorites SET user_id = ? WHERE user_id = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE invite_keys SET generated_by = ? WHERE generated_by = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE invite_keys SET claimed_by = ? WHERE claimed_by = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE journals SET author = ? WHERE author = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE journal_comments SET author = ? WHERE author = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE notifications SET recipient = ? WHERE recipient = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE notifications SET sender = ? WHERE sender = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE rating SET user = ? WHERE user = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE subscriptions SET id = ? WHERE id = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE subscriptions SET user = ? WHERE user = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE takedowns SET sender = ? WHERE sender = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE users SET new_id = ? WHERE id = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE user_old_names SET user = ? WHERE user = ?", [$temp_id, $old_id]);
+    $database->query("UPDATE videos SET author = ? WHERE author = ?", [$temp_id, $old_id]);
+}
+
+foreach ($id_mapping as $old_id => $temp_id) {
+    $new_id = $temp_id - $temp_id_base;
+    $user = $database->fetchArray($database->query("SELECT name FROM users WHERE new_id = ?", [$temp_id]))[0];
+    $database->query("UPDATE bans SET userid = ? WHERE userid = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE channel_comments SET id = ? WHERE id = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE channel_comments SET author = ? WHERE author = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE comments SET author = ? WHERE author = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE favorites SET user_id = ? WHERE user_id = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE invite_keys SET generated_by = ? WHERE generated_by = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE invite_keys SET claimed_by = ? WHERE claimed_by = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE journals SET author = ? WHERE author = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE journal_comments SET author = ? WHERE author = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE notifications SET recipient = ? WHERE recipient = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE notifications SET sender = ? WHERE sender = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE rating SET user = ? WHERE user = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE subscriptions SET id = ? WHERE id = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE subscriptions SET user = ? WHERE user = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE takedowns SET sender = ? WHERE sender = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE users SET new_id = ? WHERE new_id = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE user_old_names SET user = ? WHERE user = ?", [$new_id, $temp_id]);
+    $database->query("UPDATE videos SET author = ? WHERE author = ?", [$new_id, $temp_id]);
 
     $pfpOld = SB_DYNAMIC_PATH . '/pfp/' . $user["name"] . '.png';
-    $pfpNew = SB_DYNAMIC_PATH . '/pfp/' . $user["id"] . '.png';
-
+    $pfpNew = SB_DYNAMIC_PATH . '/pfp/' . $new_id . '.png';
     $bannerOld = SB_DYNAMIC_PATH . '/banners/' . $user["name"] . '.png';
-    $bannerNew = SB_DYNAMIC_PATH . '/banners/' . $user["id"] . '.png';
+    $bannerNew = SB_DYNAMIC_PATH . '/banners/' . $new_id . '.png';
 
     if (file_exists($pfpOld)) {
         if (!rename($pfpOld, $pfpNew)) {
