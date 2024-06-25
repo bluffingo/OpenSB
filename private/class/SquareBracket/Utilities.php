5c1c147569fd4ae075ada5ac29d12aa50c747ba6
Bluffingo <chazpelo2@gmail.com>
2024-06-25 15:51:28 -0400

    more stupid fucking shit, woo hoo!

--- private/class/SquareBracket/Utilities.php
+++ private/class/SquareBracket/Utilities.php
@@ -6,7 +6,16 @@ class Utilities
 {
     public static function get_ip_address()
     {
-        if (php_sapi_name() == "cli") return Null;
-        return $_SERVER['REMOTE_ADDR'];
+        if (php_sapi_name() == "cli") return null;
+
+        if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+            return $_SERVER['HTTP_CF_CONNECTING_IP'];
+        //} elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        //    return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } elseif (isset($_SERVER['REMOTE_ADDR'])) {
+            return $_SERVER['REMOTE_ADDR'];
+        }
+
+        return null;
     }
 }
