2ab369430f5206bc962318899d24b6eb2c98c655
Chaziz <chazpelo2@gmail.com>
2024-08-04 11:56:33 -0400

    [orange] remove webfinger code and bitqobo reference

--- private/pages/delete.php
+++ private/pages/delete.php
@@ -21,11 +21,7 @@ if ($auth->getUserID() != $data["author"]) {
     UnorganizedFunctions::Notification("This is not your submission.", "/");
 }
 
-// INSERT INTO `deleted_videos` (`autoint`, `id`, `uploaded_time`, `deleted_time`) VALUES ('1', 'sex', '231', '312');
-
 $database->query("DELETE FROM videos WHERE video_id = ?", [$id]);
-// NOTE FOR BITQOBO DEVS: for squarebracket-to-bitqobo migration, moved_to_bitqobo should be set to 1. this will
-// redirect users from squarebracket.pw to qobo.tv if they stumble upon a video that's been migrated. -chaziz 5/14/2024
 $database->query("INSERT INTO deleted_videos (id, uploaded_time, deleted_time, moved_to_bitqobo) VALUES (?,?,?,?)", [$id, $data["time"], time(), 0]);
 
 $storage->deleteSubmission($data);
