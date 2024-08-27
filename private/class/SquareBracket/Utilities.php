de33cbcd63c4d5dadea47b51ac0905438c4065b6
Chaziz <chazpelo2@gmail.com>
2024-08-26 20:26:12 -0400

    stupid fucking bullshit

--- private/class/SquareBracket/Utilities.php
+++ private/class/SquareBracket/Utilities.php
@@ -10,10 +10,30 @@ class Utilities
             return null;
         }
 
-        $cloudflare_v4 = ['199.27.128.0/21', '173.245.48.0/20', '103.21.244.0/22', '103.22.200.0/22', '103.31.4.0/22',
-            '141.101.64.0/18', '108.162.192.0/18', '190.93.240.0/20', '188.114.96.0/20', '197.234.240.0/22',
-            '198.41.128.0/17', '162.158.0.0/15', '104.16.0.0/12'];
-        $cloudflare_v6 = ['2400:cb00::/32', '2606:4700::/32', '2803:f800::/32', '2405:b500::/32', '2405:8100::/32'];
+        $cloudflare_v4 = [
+            '173.245.48.0/20',
+            '103.21.244.0/22',
+            '103.22.200.0/22',
+            '103.31.4.0/22',
+            '141.101.64.0/18',
+            '108.162.192.0/18',
+            '190.93.240.0/20',
+            '188.114.96.0/20',
+            '197.234.240.0/22',
+            '198.41.128.0/17',
+            '162.158.0.0/15',
+            '104.16.0.0/13',
+            '104.24.0.0/14',
+            '172.64.0.0/13',
+            '131.0.72.0/22'
+        ];
+        $cloudflare_v6 = [
+            '2400:cb00::/32',
+            '2606:4700::/32',
+            '2803:f800::/32',
+            '2405:b500::/32',
+            '2405:8100::/32'
+        ];
 
         $client_ip = null;
 
@@ -37,9 +57,6 @@ class Utilities
                     }
                 }
             }
-
-            // if the cloudflare ip is invalid, fall back to remote address.
-            return $_SERVER['REMOTE_ADDR'];
         }
 
         return null;
@@ -48,16 +65,17 @@ class Utilities
     private static function ip_in_range($ip, $cidr)
     {
         list($subnet, $mask) = explode('/', $cidr);
+
         if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
             $ip = ip2long($ip);
             $subnet = ip2long($subnet);
-            $mask = 0xFFFFFFFF << (32 - $mask);
+            $mask = 0xFFFFFFFF << (32 - (int)$mask);
             return ($ip & $mask) == ($subnet & $mask);
         } elseif (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
-            $subnet = inet_pton($subnet);
             $ip = inet_pton($ip);
-            $mask = str_repeat("f", $mask / 4) . str_repeat("0", 32 - $mask / 4);
-            return ($ip & $mask) == ($subnet & $mask);
+            $subnet = inet_pton($subnet);
+            $mask = str_repeat("f", (int)$mask / 4) . str_repeat("0", 32 - (int)$mask / 4);
+            return (strcmp(substr($ip, 0, strlen($mask)), substr($subnet, 0, strlen($mask))) === 0);
         }
         return false;
     }
