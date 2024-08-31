92e6db405ef7d1faec4afd94537d7765bba7aded
Chaziz <chazpelo2@gmail.com>
2024-08-31 02:40:47 -0400

    fix when userdata is used on an invalid user

--- private/class/SquareBracket/UserData.php
+++ private/class/SquareBracket/UserData.php
@@ -15,6 +15,9 @@ class UserData
         $this->data = $this->database->fetch("SELECT u.* FROM users u WHERE u.id = ?", [$id]);
         $this->followers = $this->database->fetch("SELECT COUNT(user) FROM subscriptions WHERE user = ?", [$id])['COUNT(user)'];
         $this->is_banned = $this->database->fetch("SELECT * FROM bans WHERE userid = ?", [$id]);
+        if ($this->data == null) {
+            trigger_error("Account ID $id is nonexistent.", E_USER_WARNING);
+        }
     }
 
     public function isUserBanned()
@@ -25,14 +28,26 @@ class UserData
 
     public function getUserArray(): array
     {
-        return [
-            "username" => $this->data["name"],
-            "displayname" => $this->data["title"],
-            "color" => $this->data["customcolor"],
-            "followers" => $this->followers,
-            "joined" => $this->data["joined"],
-            "connected" => $this->data["lastview"],
-            "customcolor" => $this->data["customcolor"],
-        ];
+        if ($this->data) {
+            return [
+                "username" => $this->data["name"],
+                "displayname" => $this->data["title"],
+                "color" => $this->data["customcolor"],
+                "followers" => $this->followers,
+                "joined" => $this->data["joined"],
+                "connected" => $this->data["lastview"],
+                "customcolor" => $this->data["customcolor"],
+            ];
+        } else {
+            return [
+                "username" => "InvalidUser!",
+                "displayname" => "Invalid user!",
+                "color" => "#FF0000",
+                "followers" => 0,
+                "joined" => 0,
+                "connected" => 0,
+                "customcolor" => "#FF0000",
+            ];
+        }
     }
 }
