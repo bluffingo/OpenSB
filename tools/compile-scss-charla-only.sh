c4c4b25969d23f68c715424bf74cd5934798696b
Chaziz <chazpelo2@gmail.com>
2024-10-10 03:59:04 -0400

    include compile-scss-charla-only.sh

---
+++ tools/compile-scss-charla-only.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+command=$1
+
+common_arguments="--verbose --style expanded --no-source-map --load-path=private/skins"
+
+if [ "$command" = "dev" ]; then
+  common_arguments+=" --watch --poll --fatal-deprecation"
+fi
+
+machine=$(uname -s)
+case "${machine}" in
+  CYGWIN*|MINGW*|MSYS*) machine=Windows;;
+  *)                    machine=Other;;
+esac
+
+if [ "$machine" == "Windows" ]; then
+  sass_executable="sass.bat"
+else
+  sass_executable="sass"
+fi
+
+if [ ! -d "public/assets/css" ]; then
+  mkdir -p public/assets/css || { echo "Error: Failed to create public/assets/css directory"; exit 1; }
+fi
+
+# Compile SCSS directories
+${sass_executable} ${common_arguments} private/skins/charla/scss/:public/assets/css/
+
+echo "SCSS compilation complete."
