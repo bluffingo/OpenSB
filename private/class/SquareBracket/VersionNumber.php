daa1a15a740b9f9176967188a94cbcc8d2e868d2
Chaziz <chazpelo2@gmail.com>
2024-10-31 15:02:08 -0400

    bump to 1.2.5-rc
    currently waiting for an external art request (chaz_opensb.png) to be completed

--- private/class/SquareBracket/VersionNumber.php
+++ private/class/SquareBracket/VersionNumber.php
@@ -8,7 +8,7 @@ class VersionNumber
     private string $versionString;
 
     public function __construct() {
-        $this->versionNumber = "1.2.4";
+        $this->versionNumber = "1.2.5-rc";
         $this->versionString = $this->makeVersionString();
     }
 
