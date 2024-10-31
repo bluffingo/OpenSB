460ebdffedb47f089c4f0b8169575bd5436d0f00
Chaziz <chazpelo2@gmail.com>
2024-10-31 14:53:16 -0400

    add common_shared.js

---
+++ public/assets/common_shared.js
@@ -0,0 +1,8 @@
+console.log(
+    "%cOpenSB " + opensb_version,
+    "color: blue; font-family: sans-serif; font-size: 4em;"
+);
+console.log(
+    "%cWarning: If someone instructs you to copy and paste content here, they may be attempting to access your account information.",
+    "color: red; font-family: monospace; font-size: 2em"
+);
