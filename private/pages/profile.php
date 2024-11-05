d2cb39f9bee1e02124ae2762cf0164457ff6dea4
mgnum22 <154691538+mgnum22@users.noreply.github.com>
2024-11-05 15:41:36 -0300

    Remove code by pedozoo apologist

--- private/pages/profile.php
+++
@@ -1,7 +0,0 @@
-<?php
-// YOU MIGHT BE LOOKING FOR USER.PHP -chaziz 10/25/2024
-namespace OpenSB;
-
-use SquareBracket\Utilities;
-
-if (isset($_GET['user'])) Utilities::redirect('/user/' . $_GET['user']);
