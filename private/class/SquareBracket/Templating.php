3cfa07bb97efd3759ffa0fbcf8ea52db2f28ab2b
Chaziz <chazpelo2@gmail.com>
2024-11-09 02:30:23 -0500

    remove "uploading is disabled for now"

--- private/class/SquareBracket/Templating.php
+++ private/class/SquareBracket/Templating.php
@@ -122,7 +122,7 @@ class Templating
         }
 
         $warningBannerTextIfOnNonChazizOwnedDomain = "Given recent allegations, if you do not want your uploads to remain
-        on " . $branding["name"] . ", please delete them. Uploading is currently disabled for now.";
+        on " . $branding["name"] . ", please delete them.";
 
         if ($isChazizSB) {
             $showWarningBanner = true;
@@ -179,9 +179,6 @@ class Templating
             $this->twig->addGlobal("page_url", (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
             $this->twig->addGlobal("domain", (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/");
         }
-
-        // temporary measure to update the frontend code without breaking old backend until we toss that shit out
-        $this->twig->addGlobal('areWeRunningTheNewCode', false);
     }
 
     /**
