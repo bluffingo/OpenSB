15b62e249f8cca81a8606e8debaaeae7149862f3
Chaziz <chazpelo2@gmail.com>
2024-12-17 07:35:43 -0500

    bump to 1.2.8

--- private/class/SquareBracket/VersionNumber.php
+++ private/class/SquareBracket/VersionNumber.php
@@ -8,7 +8,7 @@ class VersionNumber
     private string $versionString;
 
     public function __construct() {
-        $this->versionNumber = "1.2.7";
+        $this->versionNumber = "1.2.8";
         $this->versionString = $this->makeVersionString();
     }
 
