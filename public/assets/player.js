5be2bcd2d3e31546954ea96e179b213a1111c3b8
Chaziz <chazpelo2@gmail.com>
2026-09-08 05:49:33 -0400

    make player progress partially functional
    still no scrubbing or assets

--- public/assets/player.js
+++ public/assets/player.js
@@ -21,15 +21,56 @@ document.addEventListener("DOMContentLoaded", () => {
         });
 });
 
+function getTimeFromNumber(number) {
+    const pad = (num) => String(Math.floor(num)).padStart(2, '0');
+    
+    return [
+        number / 3600,        // h
+        (number % 3600) / 60, // m
+        number % 60           // s
+    ].map(pad).join(':');
+}
+
 function createVideoElement() {
-    let container = document.querySelector('.video-container');
-    let video = document.createElement('video');
+    const container = document.querySelector('.video-container');
+    const video = document.createElement('video');
     video.setAttribute('x-webkit-airplay', 'allow'); // allow airplay
     video.className = 'video-stream main-video';
     video.src = playerConfig.url;
     container.appendChild(video);
 
-    let playButton = document.querySelector('.player-button-play');
+    // progress bar/scrubber
+    const progressScrubber = document.querySelector('.player-scrubber');
+    const progressContainer = document.querySelector('.player-progress');
+    const progressBarElapsed = document.querySelector('.player-progress-bar-elapsed');
+
+    const progressTimeDisplay = document.querySelector('.player-time-display');
+
+    progressContainer.addEventListener('click', (event) => {
+        const rect = progressContainer.getBoundingClientRect();
+        const radius = progressScrubber.offsetWidth / 2;
+        const trackWidth = rect.width;
+
+        // figure this shit out from the event's location
+        const location = Math.min(Math.max(event.clientX - rect.left - radius, 0), trackWidth - 2 * radius);
+        const percentage = location / (trackWidth - 2 * radius);
+        video.currentTime = percentage * video.duration;
+    });
+
+    // time update
+    video.addEventListener('timeupdate', () => {
+        progressTimeDisplay.textContent = getTimeFromNumber(video.currentTime);
+        const progressPercentage = (video.currentTime / video.duration) * 100;
+
+        const scrubberRadius = progressScrubber.offsetWidth / 2;
+        const leftPx = scrubberRadius + (progressPercentage / 100) * (progressContainer.clientWidth - 2 * (scrubberRadius));
+        progressScrubber.style.left = `${leftPx}px`;
+        
+        progressBarElapsed.style.width = `${progressPercentage}%`;
+    });
+
+    // buttons
+    const playButton = document.querySelector('.player-button-play');
     playButton.addEventListener('click', () => {
         if (video.paused) {
             video.play();
